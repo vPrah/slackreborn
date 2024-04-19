c1c6000d2854e6565da12b5f9001f22b5dcceeea
David <101277472+DavidDavid1337@users.noreply.github.com>
2024-04-20 00:47:24 +0200

    uj

---
+++ src/main/java/cc/slack/features/modules/impl/movement/InvMove.java
@@ -0,0 +1,15 @@
+package cc.slack.features.modules.impl.movement;
+
+import cc.slack.features.modules.api.Category;
+import cc.slack.features.modules.api.Module;
+import cc.slack.features.modules.api.ModuleInfo;
+
+@ModuleInfo(
+        name = "InvMove",
+        category = Category.MOVEMENT
+)
+public class InvMove extends Module {
+
+    // Dont work now
+
+}
