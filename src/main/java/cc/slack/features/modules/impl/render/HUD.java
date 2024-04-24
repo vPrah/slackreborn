2971e7d1260ff25e1bb0129e8cb68b2b0618466a
dg636 <davidwu@hunterschools.org>
2024-04-24 15:33:20 -0600

    BPS counter for hud

--- src/main/java/cc/slack/features/modules/impl/render/HUD.java
+++ src/main/java/cc/slack/features/modules/impl/render/HUD.java
@@ -11,12 +11,14 @@ import cc.slack.features.modules.impl.render.hud.arraylist.IArraylist;
 import cc.slack.features.modules.impl.render.hud.arraylist.impl.*;
 import cc.slack.utils.client.mc;
 import cc.slack.utils.font.Fonts;
+import cc.slack.utils.player.MovementUtil;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import io.github.nevalackin.radbus.Listen;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.input.Keyboard;
 
 
+import static java.lang.Math.round;
 import static net.minecraft.client.gui.Gui.drawRect;
 
 @ModuleInfo(
@@ -29,11 +31,10 @@ public class HUD extends Module {
 
     private final ModeValue<String> watermarksmodes = new ModeValue<>("WaterMark-Modes", new String[]{"Classic", "Backgrounded"});
 
-    private final BooleanValue fpsdraw = new BooleanValue("ShowFPS", true);
-    private final BooleanValue bpsdraw = new BooleanValue("ShowBPS", true);
+    private final BooleanValue fpsdraw = new BooleanValue("FPS Counter", true);
+    private final BooleanValue bpsdraw = new BooleanValue("BPS Counter", true);
 
-    private final BooleanValue backgroundedraw = new BooleanValue("ShowBackgrounded", true);
-
+    private double lastBPS = 0.0;
 
     public HUD() {
         super();
@@ -47,26 +48,34 @@ public class HUD extends Module {
 
             switch (watermarksmodes.getValue()) {
                 case "Classic":
-                    Fonts.apple18.drawStringWithShadow("S", 4, 4, 0x5499C7);
-                    Fonts.apple18.drawStringWithShadow("lack", 10, 4, -1);
+                    Fonts.apple18.drawString("S", 4, 4, 0x5499C7);
+                    Fonts.apple18.drawString("lack", 10, 4, -1);
                     break;
                 case "Backgrounded":
                 drawRect(2, 2, 80, 14, 0x80000000);
-                Fonts.apple18.drawStringWithShadow("Slack " + Slack.getInstance().getInfo().getVersion(), 4, 4, 0x5499C7);
-                Fonts.apple18.drawStringWithShadow(" - " + Minecraft.getDebugFPS(), 53, 4, -1);
+                Fonts.apple18.drawString("Slack " + Slack.getInstance().getInfo().getVersion(), 4, 4, 0x5499C7);
+                Fonts.apple18.drawString(" - " + Minecraft.getDebugFPS(), 53, 4, -1);
                 break;
             }
             if (fpsdraw.getValue()) {
-                Fonts.apple20.drawStringWithShadow("FPS:  " , 4, 490, 0x5D0C1D);
-                Fonts.apple18.drawStringWithShadow(""+Minecraft.getDebugFPS(), 25, 490, -1);
+                Fonts.apple20.drawString("FPS:  " , 4, 490, 0x5D0C1D);
+                Fonts.apple18.drawString(""+Minecraft.getDebugFPS(), 25, 490, -1);
             }
 
             if (bpsdraw.getValue()) {
-                Fonts.apple20.drawStringWithShadow("BPS:  ", 50, 490, 0x5D0C1D);
+                Fonts.apple20.drawString("BPS:  ", 50, 490, 0x5D0C1D);
+                Fonts.apple18.drawString(""+getBPS(), 25, 490, -1);
 
             }
 
 //        Render2DUtil.drawImage(new ResourceLocation("slack/textures/logo/trans-512.png"), 12, 12, 32, 32, new Color(255, 255, 255, 150));
         arraylistModes.getValue().onRender(e);
+    }
+
+    private Double getBPS() {
+        double currentBPS = ((double) round((MovementUtil.getSpeed() * 20) * 100)) / 100;
+        double avgBPS = (lastBPS + currentBPS) / 2;
+        lastBPS = currentBPS;
+        return avgBPS;
     }
 }
