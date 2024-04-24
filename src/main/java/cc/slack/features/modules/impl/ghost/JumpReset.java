dce7f23b043d0fc55909daff71f8d97471f814d9
vPrah <165430229+vPrah@users.noreply.github.com>
2024-04-24 19:15:19 -0400

    Added JumpReset

---
+++ src/main/java/cc/slack/features/modules/impl/ghost/JumpReset.java
@@ -0,0 +1,31 @@
+package cc.slack.features.modules.impl.ghost;
+
+import cc.slack.events.impl.player.UpdateEvent;
+import cc.slack.features.modules.api.Category;
+import cc.slack.features.modules.api.Module;
+import cc.slack.features.modules.api.ModuleInfo;
+import cc.slack.utils.client.mc;
+import io.github.nevalackin.radbus.Listen;
+import net.minecraft.client.settings.GameSettings;
+
+@ModuleInfo(
+        name = "JumpReset",
+        category = Category.GHOST
+)
+public class JumpReset extends Module {
+    @Listen
+    public void onUpdate (UpdateEvent event) {
+        if (mc.getPlayer().hurtTime >= 8) {
+            mc.getGameSettings().keyBindJump.pressed = true;
+        }
+        if (mc.getPlayer().hurtTime >= 7) {
+            mc.getGameSettings().keyBindForward.pressed = true;
+        } else if (mc.getPlayer().hurtTime >= 4) {
+            mc.getGameSettings().keyBindJump.pressed = false;
+            mc.getGameSettings().keyBindForward.pressed = false;
+        } else if (mc.getPlayer().hurtTime > 1){
+            mc.getGameSettings().keyBindForward.pressed = GameSettings.isKeyDown(mc.getGameSettings().keyBindForward);
+            mc.getGameSettings().keyBindJump.pressed = GameSettings.isKeyDown(mc.getGameSettings().keyBindJump);
+        }
+    }
+}
