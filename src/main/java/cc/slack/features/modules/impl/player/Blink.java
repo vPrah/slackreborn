36efd261fe0cc15b48b7d10d23b749a11ca7c7c0
dg636 <davidwu@hunterschools.org>
2024-04-23 23:36:31 -0600

    Blink -> blinkutils

--- src/main/java/cc/slack/features/modules/impl/player/Blink.java
+++ src/main/java/cc/slack/features/modules/impl/player/Blink.java
@@ -1,25 +1,11 @@
 package cc.slack.features.modules.impl.player;
 
-import cc.slack.events.impl.network.PacketEvent;
 import cc.slack.features.modules.api.Category;
 import cc.slack.features.modules.api.Module;
 import cc.slack.features.modules.api.ModuleInfo;
 import cc.slack.features.modules.api.settings.impl.BooleanValue;
-import cc.slack.utils.client.mc;
-import cc.slack.utils.network.PacketUtil;
-import io.github.nevalackin.radbus.Listen;
-import net.minecraft.network.Packet;
-import net.minecraft.network.handshake.client.C00Handshake;
-import net.minecraft.network.login.client.C00PacketLoginStart;
-import net.minecraft.network.login.server.S00PacketDisconnect;
-import net.minecraft.network.play.client.C00PacketKeepAlive;
-import net.minecraft.network.play.server.S19PacketEntityStatus;
-import net.minecraft.network.play.server.S02PacketChat;
-import net.minecraft.network.play.server.S3BPacketScoreboardObjective;
-import net.minecraft.network.play.server.S3EPacketTeams;
-import net.minecraft.network.status.server.S00PacketServerInfo;
+import cc.slack.utils.player.BlinkUtil;
 
-import java.util.concurrent.CopyOnWriteArrayList;
 
 @ModuleInfo(
         name = "Blink",
@@ -27,8 +13,6 @@ import java.util.concurrent.CopyOnWriteArrayList;
 )
 public class Blink extends Module {
 
-    private final CopyOnWriteArrayList<Packet> clientPackets = new CopyOnWriteArrayList<>();
-    private final CopyOnWriteArrayList<Packet> serverPackets = new CopyOnWriteArrayList<>();
 
     private final BooleanValue outbound = new BooleanValue("Outbound", true);
     private final BooleanValue inbound = new BooleanValue("Inbound", false);
@@ -40,47 +24,13 @@ public class Blink extends Module {
 
     @Override
     public void onEnable() {
-        clientPackets.clear();
-        serverPackets.clear();
+        BlinkUtil.enable(inbound.getValue(), outbound.getValue());
     }
 
     @Override
     public void onDisable() {
-        clientPackets.forEach(packet -> {
-            PacketUtil.sendNoEvent(packet);
-            clientPackets.remove(packet);
-        });
-
-        serverPackets.forEach(packet -> {
-            packet.processPacket(mc.getMinecraft().getNetHandler().getNetworkManager().getNetHandler());
-            serverPackets.remove(packet);
-        });
+        BlinkUtil.disable();
     }
 
-    @Listen
-    public void onPacket(PacketEvent event) {
-        switch (event.getDirection()) {
-            case OUTGOING:
-                if (outbound.getValue()) {
-                    if (!(event.getPacket() instanceof C00PacketKeepAlive || event.getPacket() instanceof C00Handshake ||
-                            event.getPacket() instanceof C00PacketLoginStart)) {
-                        clientPackets.add(event.getPacket());
-                        event.cancel();
-                    }
-                }
-                break;
-            case INCOMING:
-                if (mc.getPlayer() == null || mc.getWorld() == null) return;
-                if (inbound.getValue()) {
-                    if (!(event.getPacket() instanceof S00PacketDisconnect ||
-                            event.getPacket() instanceof S00PacketServerInfo || event.getPacket() instanceof S3EPacketTeams ||
-                            event.getPacket() instanceof S19PacketEntityStatus || event.getPacket() instanceof S02PacketChat ||
-                            event.getPacket() instanceof S3BPacketScoreboardObjective)) {
-                        serverPackets.add(event.getPacket());
-                        event.cancel();
-                    }
-                }
-                break;
-        }
-    }
+
 }
