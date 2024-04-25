87a0d7e5924ee117de408e9abf151d1128ecf1a1
vPrah <165430229+vPrah@users.noreply.github.com>
2024-04-24 20:15:45 -0400

    Scaffold settings

--- src/main/java/cc/slack/features/modules/impl/world/Scaffold.java
+++ src/main/java/cc/slack/features/modules/impl/world/Scaffold.java
@@ -47,6 +47,11 @@ public class Scaffold extends Module {
     private final BooleanValue strafeFix = new BooleanValue("Movement Correction", true);
     private final ModeValue<String> towerMode = new ModeValue<>("Tower Mode", new String[] {"Vanilla", "Vulcan", "Off"});
     private final BooleanValue towerNoMove = new BooleanValue("Tower No Move", false);
+
+    public Scaffold() {
+        addSettings(rotationMode, keepRotationTicks, sprintMode, strafeFix, towerMode, towerNoMove);
+    }
+
     /*
     TODO:
     eagle / safewalk
