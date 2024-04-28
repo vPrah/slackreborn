54b0f7d0ce340b78342639424f15338c73cb390f
dg636 <davidwu@hunterschools.org>
2024-04-28 15:07:37 -0600

    switch to attack event

--- src/main/java/cc/slack/features/modules/impl/render/TargetHUD.java
+++ src/main/java/cc/slack/features/modules/impl/render/TargetHUD.java
@@ -1,6 +1,6 @@
 package cc.slack.features.modules.impl.render;
 
-import cc.slack.events.impl.network.PacketEvent;
+import cc.slack.events.impl.player.AttackEvent;
 import cc.slack.events.impl.player.UpdateEvent;
 import cc.slack.events.impl.render.RenderEvent;
 import cc.slack.features.modules.api.Category;
@@ -56,14 +56,9 @@ public class TargetHUD extends Module {
     }
 
     @Listen
-    public void onPacket(PacketEvent event) {
-            if (event.getPacket() instanceof C02PacketUseEntity) {
-                C02PacketUseEntity wrapper = (C02PacketUseEntity) event.getPacket();
-                if (wrapper.getEntityFromWorld(mc.getWorld()) instanceof EntityPlayer && wrapper.getAction() == C02PacketUseEntity.Action.ATTACK) {
-                    ticksSinceAttack = 0;
-                    player = (EntityPlayer) wrapper.getEntityFromWorld(mc.getWorld());
-                }
-            }
+    public void onAttack(AttackEvent event) {
+        ticksSinceAttack = 0;
+        player = (EntityPlayer) event.getTargetEntity();
     }
 
     @Listen
