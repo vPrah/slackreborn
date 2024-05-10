f64a699c55457ecfc4503d2049903a3560c5d6d7
dg636 <114008944+dg636@users.noreply.github.com>
2024-05-10 12:33:26 -0400

    Update LegitScaffold.java
    Signed-off-by: dg636 <114008944+dg636@users.noreply.github.com>

--- src/main/java/cc/slack/features/modules/impl/ghost/LegitScaffold.java
+++ src/main/java/cc/slack/features/modules/impl/ghost/LegitScaffold.java
@@ -7,8 +7,10 @@ import cc.slack.features.modules.api.Category;
 import cc.slack.features.modules.api.Module;
 import cc.slack.features.modules.api.ModuleInfo;
 import cc.slack.features.modules.api.settings.impl.BooleanValue;
+import cc.slack.features.modules.api.settings.impl.NumberValue;
 import cc.slack.utils.client.mc;
 import cc.slack.utils.player.PlayerUtil;
+import cc.slack.utils.other.TimeUtil;
 import io.github.nevalackin.radbus.Listen;
 import net.minecraft.client.settings.GameSettings;
 
@@ -18,16 +20,26 @@ import net.minecraft.client.settings.GameSettings;
 )
 public class LegitScaffold extends Module {
 
+    private final NumberValue<Integer> sneakTime = new NumberValue<>("Sneak Time", 60, 0, 300, 20);    
     private final BooleanValue onlyGround = new BooleanValue("Only Ground", true);
+    private final BooleanValue holdSneak = new BooleanValue("Hold Sneak", false);
 
     private boolean shouldSneak = false;
+    private final TimeUtil sneakTimer = new TimeUtil();
 
     @SuppressWarnings("unused")
     @Listen
     public void onUpdate (UpdateEvent event) {
         if (mc.getCurrentScreen() != null) return;
-        shouldSneak = PlayerUtil.isOverAir() && (!onlyGround.getValue() || mc.getPlayer().onGround);
-        mc.getGameSettings().keyBindSneak.pressed = GameSettings.isKeyDown(mc.getGameSettings().keyBindSneak) || shouldSneak;
+        if (PlayerUtil.isOverAir() && (!onlyGround.getValue() || mc.getPlayer().onGround) && mc.getPlayer().motionY < 0.1) {
+            sneakTimer.reset();
+        }
+        shouldSneak = !sneakTime.hasReached((long) sneakTime.getValue());
 
+        if (holdSneak.getValue()) {
+            mc.getGameSettings().keyBindSneak.pressed = GameSettings.isKeyDown(mc.getGameSettings().keyBindSneak) && shouldSneak;
+        } else {
+            mc.getGameSettings().keyBindSneak.pressed = GameSettings.isKeyDown(mc.getGameSettings().keyBindSneak) || shouldSneak;
+        }
     }
 }
