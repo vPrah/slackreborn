6d302fed652795164b64770e795410365536091a
vPrah <165430229+vPrah@users.noreply.github.com>
2024-05-15 19:37:59 -0400

    New events for PacketUtil

--- src/main/java/cc/slack/utils/network/PacketUtil.java
+++ src/main/java/cc/slack/utils/network/PacketUtil.java
@@ -2,6 +2,10 @@ package cc.slack.utils.network;
 
 import cc.slack.utils.client.mc;
 import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.C07PacketPlayerDigging;
+import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 
 public final class PacketUtil extends mc {
 
@@ -16,8 +20,28 @@ public final class PacketUtil extends mc {
     public static void sendNoEvent(Packet<?> packet) {
         getNetHandler().getNetworkManager().sendPacketNoEvent(packet);
     }
-
     public static void sendNoEvent(Packet<?> packet, int iterations) {
         for (int i = 0; i < iterations; i++) sendNoEvent(packet);
     }
+
+    public static void sendBlocking(boolean callEvent, boolean place) {
+        C08PacketPlayerBlockPlacement packet;
+        C08PacketPlayerBlockPlacement c08PacketPlayerBlockPlacement = packet = place ? new C08PacketPlayerBlockPlacement(new BlockPos(-1, -1, -1), 255, mc.getPlayer().getHeldItem(), 0.0f, 0.0f, 0.0f) : new C08PacketPlayerBlockPlacement(mc.getPlayer().getHeldItem());
+        if (callEvent) {
+            PacketUtil.send(packet);
+        } else {
+            PacketUtil.sendNoEvent(packet);
+        }
+    }
+
+    public static void releaseUseItem(boolean callEvent) {
+        C07PacketPlayerDigging packet = new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN);
+        if (callEvent) {
+            PacketUtil.send(packet);
+        } else {
+            PacketUtil.sendNoEvent(packet);
+        }
+    }
+
+
 }
