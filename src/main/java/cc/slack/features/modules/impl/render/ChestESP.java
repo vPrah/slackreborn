7203763ac00b289bd85886dcfbae5b07f088bb13
vPrah <165430229+vPrah@users.noreply.github.com>
2024-05-18 08:40:36 -0400

    Reapply "Update ChestESP.java"
    This reverts commit a7a4567a50f9dc4d7bddc0d510098a6aabc4ddb2.

--- src/main/java/cc/slack/features/modules/impl/render/ChestESP.java
+++ src/main/java/cc/slack/features/modules/impl/render/ChestESP.java
@@ -2,13 +2,32 @@
 
 package cc.slack.features.modules.impl.render;
 
+import cc.slack.Slack;
+import cc.slack.features.modules.impl.render.ESP;
 import cc.slack.features.modules.api.Category;
 import cc.slack.features.modules.api.Module;
 import cc.slack.features.modules.api.ModuleInfo;
-
+import cc.slack.events.impl.render.RenderEvent;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.AxisAlignedBB;
+import java.util.ArrayList;
 @ModuleInfo(
         name = "ChestESP",
         category = Category.RENDER
 )
 public class ChestESP extends Module {
+
+    public ArrayList<BlockPos> chestBoundingBoxes = new ArrayList();
+
+    @Listen
+    public void onRender(RenderEvent event) {
+        if (event.getState() != RenderEvent.State.RENDER_3D) return;
+
+        for (BlockPos bp : chestBoundingBoxes) {
+            Slack.getInstance().getModuleManager().getInstance(ESP.class).drawAABB(AxisAlignedBB.fromBounds(bp.getX(), bp.getY(), bp.getZ(), bp.getX() + 1, bp.getY() + 1, bp.getZ() + 1));
+        }
+
+        chestBoundingBoxes.clear()
+    }
+    
 }
