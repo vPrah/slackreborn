06328d23076a9f43baa0bd75e944a9d882ab99a7
Nerek <laruxtopiaz@gmaill.com>
2024-05-22 17:30:48 +0200

    MainMenu Updated x2

---
+++ src/main/java/cc/slack/ui/menu/MainMenu.java
@@ -0,0 +1,67 @@
+package cc.slack.ui.menu;
+
+import cc.slack.Slack;
+import cc.slack.ui.alt.GuiAltLogin;
+import cc.slack.utils.client.ClientInfo;
+import net.minecraft.client.gui.*;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.ResourceLocation;
+
+import java.awt.*;
+import java.io.IOException;
+
+public class MainMenu extends GuiScreen {
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        mc.getTextureManager().bindTexture(new ResourceLocation("slack/menu/test.jpeg"));
+        drawModalRectWithCustomSizedTexture(0, 0,0,0, this.width, this.height, this.width, this.height);
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(width/2f, height/2f - mc.MCfontRenderer.FONT_HEIGHT/2f, 0);
+        GlStateManager.scale(3, 3, 1);
+        GlStateManager.translate(-(width/1.69f), -(height/1.77),0);
+      //  this.drawCenteredString(mc.MCfontRenderer, Slack.getInstance().info.name, width/2, height/13- mc.MCfontRenderer.FONT_HEIGHT/2, 0x3435353F);
+        GlStateManager.popMatrix();
+        Gui.drawRect(0, 0, 140 , this.height, new Color(0,0,0,170).getRGB());
+        super.drawScreen(mouseX, mouseY, partialTicks);
+
+
+    }
+
+
+    @Override
+    public void initGui() {
+        this.menuList.add(new MainMenuButton(1, - 30, height / 2 - 40, "Singleplayer"));
+        this.menuList.add(new MainMenuButton(2, - 30, height / 2 - 15, "MultiPlayer"));
+        this.menuList.add(new MainMenuButton(3, - 30, height / 2 + 10, "Settings"));
+        this.menuList.add(new MainMenuButton(4, - 30, height / 2 + 35, "Alt Manager"));
+        this.menuList.add(new MainMenuButton(5, - 30, height / 2 + 60, "Shutdown"));
+
+        super.initGui();
+    }
+
+    @Override
+    protected void actionPerformedMenu(MainMenuButton buttonMenu) throws IOException {
+        super.actionPerformedMenu(buttonMenu);
+
+        if(buttonMenu.id == 1) {
+            mc.displayGuiScreen(new GuiSelectWorld(this));
+        }
+
+        if(buttonMenu.id == 2) {
+            mc.displayGuiScreen(new GuiMultiplayer(this));
+        }
+
+        if(buttonMenu.id == 3) {
+            mc.displayGuiScreen(new GuiOptions(this, this.mc.gameSettings));
+        }
+
+        if(buttonMenu.id == 4) {
+            mc.displayGuiScreen(new GuiAltLogin(this));
+        }
+
+        if(buttonMenu.id == 5) {
+            mc.shutdown();
+        }
+    }
+}
