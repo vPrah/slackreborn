3d92e38515b86967e926d3cf169d71dfcd15c445
Cvrwed <qspoofing@gmail.com>
2024-05-22 20:06:12 -0500

    fix fps issue

--- src/main/java/cc/slack/features/modules/impl/other/RichPresence.java
+++ src/main/java/cc/slack/features/modules/impl/other/RichPresence.java
@@ -1,5 +1,10 @@
 package cc.slack.features.modules.impl.other;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import cc.slack.Slack;
 import cc.slack.events.impl.game.TickEvent;
 import cc.slack.features.modules.api.Category;
@@ -19,51 +24,51 @@ import net.arikia.dev.drpc.DiscordRichPresence;
 public class RichPresence extends Module {
 
     private final BooleanValue showNickname = new BooleanValue("Show Nickname", true);
-
-    private boolean started;
-
-    String str2 = "";
+    private final AtomicBoolean started = new AtomicBoolean(false);
+    private ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private final StringBuilder str2 = new StringBuilder();
 
     public RichPresence() {
-       addSettings(showNickname);
+        addSettings(showNickname);
     }
-
-
-
-    @Listen
-    public void onTick (TickEvent event) {
-
-        if (showNickname.getValue()) {
-            String playerName = mc.getPlayer() != null ? mc.getPlayer().getNameClear() : mc.getMinecraft().session.getUsername();
-            str2 += "IGN: " + playerName + "\n";
-        }
-
-        if (!started) {
-            final DiscordRichPresence.Builder builder = new DiscordRichPresence.Builder(str2) {{
-                setDetails("Playing Slack Client");
-                setBigImage("slack", "Slack Client " + Slack.getInstance().info.getVersion());
-                setStartTimestamps(System.currentTimeMillis());
-            }};
-
-            DiscordRPC.discordUpdatePresence(builder.build());
-
-            final DiscordEventHandlers handlers = new DiscordEventHandlers();
-            DiscordRPC.discordInitialize("1241556030664736788", handlers, true);
-
-            new Thread(() -> {
-                while (isToggle()) {
-                    DiscordRPC.discordRunCallbacks();
-                }
-            }, "Discord RPC Callback").start();
-
-            started = true;
-        }
-    };
 
     @Override
     public void onDisable() {
         DiscordRPC.discordShutdown();
-        started = false;
+        started.set(false);
+    }
+
+    @Listen
+    public void onTick(TickEvent event) {
+        if (showNickname.getValue()) {
+            String playerName = mc.getPlayer() != null ? mc.getPlayer().getNameClear() : mc.getMinecraft().session.getUsername();
+            str2.setLength(0);
+            str2.append("IGN: ").append(playerName).append("\n");
+        }
+        
+        startDiscordThread();
+    }
+
+    private void startDiscordThread() {
+        if (started.compareAndSet(false, true)) {
+        	scheduler.execute(() -> {
+                if (!isToggle()) {
+                    return;
+                }
+
+               final DiscordRichPresence.Builder builder = new DiscordRichPresence.Builder(str2.toString())
+                    .setDetails("Playing Slack Client")
+                    .setBigImage("slack", "Slack Client " + Slack.getInstance().info.getVersion())
+                    .setStartTimestamps(System.currentTimeMillis());
+
+                DiscordRPC.discordUpdatePresence(builder.build());
+                DiscordRPC.discordRunCallbacks();
+
+        	});
+
+            DiscordEventHandlers handlers = new DiscordEventHandlers();
+            DiscordRPC.discordInitialize("1241556030664736788", handlers, true);
+        }
     }
 
 }
