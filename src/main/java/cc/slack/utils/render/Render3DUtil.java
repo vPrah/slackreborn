e760442e7f8ee94d4da277b52e14eff8ecb4324e
vPrah <165430229+vPrah@users.noreply.github.com>
2024-05-25 08:51:47 -0400

    ChinaHat Better code

--- src/main/java/cc/slack/utils/render/Render3DUtil.java
+++ src/main/java/cc/slack/utils/render/Render3DUtil.java
@@ -18,6 +18,7 @@ import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import org.lwjgl.opengl.GL11;
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -80,6 +81,34 @@ public final class Render3DUtil extends mc {
         worldrenderer.pos(boundingBox.maxX, boundingBox.minY, boundingBox.minZ).endVertex();
 
         tessellator.draw();
+    }
+
+    public static void drawHat(final Entity entity, final double radius, final float partialTicks, final int points, final float width, final float yAdd, final int color) {
+        GL11.glPushMatrix();
+        GL11.glDisable(3553);
+        GL11.glEnable(2848);
+        GL11.glHint(3154, 4354);
+        GL11.glDepthMask(false);
+        GL11.glLineWidth(width);
+        GL11.glEnable(3042);
+        GL11.glBlendFunc(770, 771);
+        GL11.glEnable(2929);
+        GL11.glBegin(3);
+        final double x = Render2DUtil.interpolate(entity.prevPosX, entity.posX, partialTicks) - RenderManager.viewerPosX;
+        final double y = Render2DUtil.interpolate(entity.prevPosY + yAdd, entity.posY + yAdd, partialTicks) - RenderManager.viewerPosY;
+        final double z = Render2DUtil.interpolate(entity.prevPosZ, entity.posZ, partialTicks) - RenderManager.viewerPosZ;
+        GL11.glColor4f(new Color(color).getRed() / 255.0f, new Color(color).getGreen() / 255.0f, new Color(color).getBlue() / 255.0f, 0.15f);
+        for (int i = 0; i <= points; ++i) {
+            GL11.glVertex3d(x + radius * Math.cos(i * 3.141592653589793 * 2.0 / points), y, z + radius * Math.sin(i * 3.141592653589793 * 2.0 / points));
+        }
+        GL11.glEnd();
+        GL11.glDepthMask(true);
+        GL11.glDisable(3042);
+        GL11.glEnable(2929);
+        GL11.glDisable(2848);
+        GL11.glEnable(2929);
+        GL11.glEnable(3553);
+        GL11.glPopMatrix();
     }
 
     public static void drawRenderItemPhysics(Entity par1Entity, double x, double y, double z, float par8, float par9) {
