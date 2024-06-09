db349b68e8d4a00c013d076b1cbc2f6182d6f509
vPrah <165430229+vPrah@users.noreply.github.com>
2024-06-09 13:08:08 -0400

    Added ChestESP module

--- src/main/java/cc/slack/features/modules/impl/render/ChestESP.java
+++ src/main/java/cc/slack/features/modules/impl/render/ChestESP.java
@@ -6,6 +6,8 @@ import cc.slack.features.modules.api.Category;
 import cc.slack.features.modules.api.Module;
 import cc.slack.features.modules.api.ModuleInfo;
 import cc.slack.events.impl.render.RenderEvent;
+import cc.slack.features.modules.api.settings.impl.BooleanValue;
+import cc.slack.features.modules.api.settings.impl.NumberValue;
 import cc.slack.utils.client.mc;
 import cc.slack.utils.render.ColorUtil;
 import io.github.nevalackin.radbus.Listen;
@@ -28,5 +30,137 @@ import java.awt.*;
 )
 public class ChestESP extends Module {
 
+    private final NumberValue<Float> lineWidth = new NumberValue<>("Line Width", 6f, 1f, 10f, 0.1f);
+    private final BooleanValue clientValue = new BooleanValue("Client Theme", true);
+    private final BooleanValue rgbValue = new BooleanValue("Rainbow", false);
+    private final NumberValue<Integer> redValue = new NumberValue<>("Red", 0, 0, 255, 1);
+    private final NumberValue<Integer> greenValue = new NumberValue<>("Green", 255, 0, 255, 1);
+    private final NumberValue<Integer> blueValue = new NumberValue<>("Blue", 255, 0, 255, 1);
+    private final NumberValue<Integer> alphaValue = new NumberValue<>("Alpha", 200, 0, 255, 1);
 
+    public ChestESP() {
+        addSettings(lineWidth ,clientValue, rgbValue, redValue, greenValue, blueValue, alphaValue);
+    }
+
+    @Listen
+    public void onRender (RenderEvent event) {
+        Color ct = ColorUtil.getColor();
+        for (TileEntity tileEntity : mc.getWorld().loadedTileEntityList) {
+            if(tileEntity instanceof TileEntityChest) {
+                renderOne(2.5f);
+                TileEntityRendererDispatcher.instance.renderTileEntity(tileEntity, event.getPartialTicks(), -1);
+                renderTwo();
+                TileEntityRendererDispatcher.instance.renderTileEntity(tileEntity, event.getPartialTicks(), -1);
+                renderThree();
+                TileEntityRendererDispatcher.instance.renderTileEntity(tileEntity, event.getPartialTicks(), -1);
+                if (clientValue.getValue()) {
+                    renderFour(ct);
+                } else {
+                    renderFour((!rgbValue.getValue()) ? new Color(redValue.getValue(), greenValue.getValue(), blueValue.getValue(), alphaValue.getValue()) : ColorUtil.rainbow(-100, 1.0f, 0.47f));
+                }
+                TileEntityRendererDispatcher.instance.renderTileEntity(tileEntity, event.getPartialTicks(), -1);
+                renderFive();
+                GL11.glColor4f(1, 1, 1, 1);
+                GlStateManager.resetColor();
+            }
+        }
+    }
+
+    public void renderOne(float lineWidth) {
+        checkSetupFBO();
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+        GL11.glDisable(GL11.GL_ALPHA_TEST);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glLineWidth(this.lineWidth.getValue());
+        GL11.glEnable(GL11.GL_LINE_SMOOTH);
+        GL11.glEnable(GL11.GL_STENCIL_TEST);
+        GL11.glClear(GL11.GL_STENCIL_BUFFER_BIT);
+        GL11.glClearStencil(0xF);
+        GL11.glStencilFunc(GL11.GL_NEVER, 1, 0xF);
+        GL11.glStencilOp(GL11.GL_REPLACE, GL11.GL_REPLACE, GL11.GL_REPLACE);
+        GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
+    }
+
+    public void renderTwo() {
+        GL11.glStencilFunc(GL11.GL_NEVER, 0, 0xF);
+        GL11.glStencilOp(GL11.GL_REPLACE, GL11.GL_REPLACE, GL11.GL_REPLACE);
+        GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_FILL);
+    }
+
+    public void renderThree() {
+        GL11.glStencilFunc(GL11.GL_EQUAL, 1, 0xF);
+        GL11.glStencilOp(GL11.GL_KEEP, GL11.GL_KEEP, GL11.GL_KEEP);
+        GL11.glPolygonMode(GL11.GL_FRONT_AND_BACK, GL11.GL_LINE);
+    }
+
+    public void renderFour(Color color) {
+        GL11.glColor4d(color.getRed() / 255F, color.getGreen() / 255F, color.getBlue() / 255F, color.getAlpha() / 255F);
+        GL11.glDepthMask(false);
+        GL11.glDisable(GL11.GL_DEPTH_TEST);
+        GL11.glEnable(GL11.GL_POLYGON_OFFSET_LINE);
+        GL11.glPolygonOffset(1.0F, -2000000F);
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
+    }
+
+    public void renderFive() {
+        GL11.glPolygonOffset(1.0F, 2000000F);
+        GL11.glDisable(GL11.GL_POLYGON_OFFSET_LINE);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glDepthMask(true);
+        GL11.glDisable(GL11.GL_STENCIL_TEST);
+        GL11.glDisable(GL11.GL_LINE_SMOOTH);
+        GL11.glHint(GL11.GL_LINE_SMOOTH_HINT, GL11.GL_DONT_CARE);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glEnable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        GL11.glEnable(GL11.GL_ALPHA_TEST);
+        GL11.glPopAttrib();
+    }
+
+    public void checkSetupFBO() {
+        // Gets the FBO of Minecraft
+        final Framebuffer fbo = Minecraft.getMinecraft().getFramebuffer();
+
+        // Check if FBO isn't null
+        if(fbo != null) {
+            // Checks if screen has been resized or new FBO has been created
+            if(fbo.depthBuffer > -1) {
+                // Sets up the FBO with depth and stencil extensions (24/8 bit)
+                setupFBO(fbo);
+                // Reset the ID to prevent multiple FBO's
+                fbo.depthBuffer = -1;
+            }
+        }
+    }
+
+    /**
+     * Sets up the FBO with depth and stencil
+     *
+     * @param fbo Framebuffer
+     */
+    private void setupFBO(Framebuffer fbo) {
+        // Deletes old render buffer extensions such as depth
+        // Args: Render Buffer ID
+        EXTFramebufferObject.glDeleteRenderbuffersEXT(fbo.depthBuffer);
+        // Generates a new render buffer ID for the depth and stencil extension
+        final int stencil_depth_buffer_ID = EXTFramebufferObject.glGenRenderbuffersEXT();
+        // Binds new render buffer by ID
+        // Args: Target (GL_RENDERBUFFER_EXT), ID
+        EXTFramebufferObject.glBindRenderbufferEXT(EXTFramebufferObject.GL_RENDERBUFFER_EXT, stencil_depth_buffer_ID);
+        // Adds the depth and stencil extension
+        // Args: Target (GL_RENDERBUFFER_EXT), Extension (GL_DEPTH_STENCIL_EXT),
+        // Width, Height
+        EXTFramebufferObject.glRenderbufferStorageEXT(EXTFramebufferObject.GL_RENDERBUFFER_EXT, EXTPackedDepthStencil.GL_DEPTH_STENCIL_EXT, Minecraft.getMinecraft().displayWidth, Minecraft.getMinecraft().displayHeight);
+        // Adds the stencil attachment
+        // Args: Target (GL_FRAMEBUFFER_EXT), Attachment
+        // (GL_STENCIL_ATTACHMENT_EXT), Target (GL_RENDERBUFFER_EXT), ID
+        EXTFramebufferObject.glFramebufferRenderbufferEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT, EXTFramebufferObject.GL_STENCIL_ATTACHMENT_EXT, EXTFramebufferObject.GL_RENDERBUFFER_EXT, stencil_depth_buffer_ID);
+        // Adds the depth attachment
+        // Args: Target (GL_FRAMEBUFFER_EXT), Attachment
+        // (GL_DEPTH_ATTACHMENT_EXT), Target (GL_RENDERBUFFER_EXT), ID
+        EXTFramebufferObject.glFramebufferRenderbufferEXT(EXTFramebufferObject.GL_FRAMEBUFFER_EXT, EXTFramebufferObject.GL_DEPTH_ATTACHMENT_EXT, EXTFramebufferObject.GL_RENDERBUFFER_EXT, stencil_depth_buffer_ID);
+    }
 }
