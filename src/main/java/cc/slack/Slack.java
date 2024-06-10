ec004c11182a320c6a101291737f8a54bdbb78ed
Dg636 <dg636gamez@gmail.com>
2024-06-09 20:53:45 -0400

    efw

--- src/main/java/cc/slack/Slack.java
+++ src/main/java/cc/slack/Slack.java
@@ -46,7 +46,7 @@ public class Slack {
     public final String DiscordServer = "https://discord.gg/nwR9AyjnK8";
     public final String Website = "https://slackclient.github.io/";
     public final String codename = "Ariana's";
-    public final boolean noREQHwid = getInfo().type == ClientInfo.VersionType.DEVELOPER;
+    public final boolean noREQHwid = info.type == ClientInfo.VersionType.DEVELOPER;
 
     public void start() {
         PrintUtil.print("Initializing " + info.getName());
