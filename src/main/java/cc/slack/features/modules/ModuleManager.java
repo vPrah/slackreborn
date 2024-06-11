f1f720868fab7401a4eca12ed3ebbcd54ba513fb
Dg636 <dg636gamez@gmail.com>
2024-06-11 16:23:29 -0400

    bye

--- src/main/java/cc/slack/features/modules/ModuleManager.java
+++ src/main/java/cc/slack/features/modules/ModuleManager.java
@@ -153,7 +153,6 @@ public class ModuleManager {
                     new TNTHelper(),
                     new LegitMode(),
                     new LagbackChecker(),
-                    new PacketDebugger()
 
             );
             
