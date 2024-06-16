62f284e8864c23801e672b80cb4799996ecc3a46
vPrah <165430229+vPrah@users.noreply.github.com>
2024-06-16 19:40:12 -0400

    New DrawRoundedRect

--- src/main/java/cc/slack/utils/render/RenderUtil.java
+++ src/main/java/cc/slack/utils/render/RenderUtil.java
@@ -549,59 +549,46 @@ public final class RenderUtil implements IMinecraft {
         Gui.drawRect(x, y, x2, y2, color);
     }
 
-    public static void drawRoundedRect(float paramXStart, float paramYStart, float paramXEnd, float paramYEnd, float radius, int color) {
-        drawRoundedRect(paramXStart, paramYStart, paramXEnd, paramYEnd, radius, color, true);
-    }
 
-    public static void drawRoundedRect(float paramXStart, float paramYStart, float paramXEnd, float paramYEnd, float radius, int color, boolean popPush) {
-        float alpha = (color >> 24 & 0xFF) / 255.0F;
-        float red = (color >> 16 & 0xFF) / 255.0F;
-        float green = (color >> 8 & 0xFF) / 255.0F;
-        float blue = (color & 0xFF) / 255.0F;
-
-        float z;
-        if (paramXStart > paramXEnd) {
-            z = paramXStart;
-            paramXStart = paramXEnd;
-            paramXEnd = z;
+    public static void drawRoundedRect(double x, double y, double x1, double y1, double radius, int color) {
+        int i;
+        GL11.glPushAttrib(0);
+        GL11.glScaled(0.5, 0.5, 0.5);
+        x *= 2.0;
+        y *= 2.0;
+        x1 *= 2.0;
+        y1 *= 2.0;
+        GL11.glEnable(3042);
+        GL11.glDisable(3553);
+        float a = (float)(color >> 24 & 0xFF) / 255.0f;
+        float r = (float)(color >> 16 & 0xFF) / 255.0f;
+        float g = (float)(color >> 8 & 0xFF) / 255.0f;
+        float b = (float)(color & 0xFF) / 255.0f;
+        GL11.glColor4f(r, g, b, a);
+        GL11.glEnable(2848);
+        GL11.glBegin(9);
+        for (i = 0; i <= 90; i += 3) {
+            GL11.glVertex2d(x + radius + Math.sin((double)i * Math.PI / 180.0) * radius * -1.0, y + radius + Math.cos((double)i * Math.PI / 180.0) * radius * -1.0);
         }
-
-        if (paramYStart > paramYEnd) {
-            z = paramYStart;
-            paramYStart = paramYEnd;
-            paramYEnd = z;
+        for (i = 90; i <= 180; i += 3) {
+            GL11.glVertex2d(x + radius + Math.sin((double)i * Math.PI / 180.0) * radius * -1.0, y1 - radius + Math.cos((double)i * Math.PI / 180.0) * radius * -1.0);
         }
-
-        double x1 = (paramXStart + radius);
-        double y1 = (paramYStart + radius);
-        double x2 = (paramXEnd - radius);
-        double y2 = (paramYEnd - radius);
-
-        if (popPush) glPushMatrix();
-        glEnable(GL_BLEND);
-        glDisable(GL_TEXTURE_2D);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glEnable(GL_LINE_SMOOTH);
-        glLineWidth(1);
-
-        glColor4f(red, green, blue, alpha);
-        glBegin(GL_POLYGON);
-
-        double degree = Math.PI / 180;
-        for (double i = 0; i <= 90; i += 1)
-            glVertex2d(x2 + Math.sin(i * degree) * radius, y2 + Math.cos(i * degree) * radius);
-        for (double i = 90; i <= 180; i += 1)
-            glVertex2d(x2 + Math.sin(i * degree) * radius, y1 + Math.cos(i * degree) * radius);
-        for (double i = 180; i <= 270; i += 1)
-            glVertex2d(x1 + Math.sin(i * degree) * radius, y1 + Math.cos(i * degree) * radius);
-        for (double i = 270; i <= 360; i += 1)
-            glVertex2d(x1 + Math.sin(i * degree) * radius, y2 + Math.cos(i * degree) * radius);
-        glEnd();
-        glColor4f(1, 1, 1, 1);
-        glEnable(GL_TEXTURE_2D);
-//        glDisable(GL_BLEND);
-        glDisable(GL_LINE_SMOOTH);
-        if (popPush) glPopMatrix();
+        for (i = 0; i <= 90; i += 3) {
+            GL11.glVertex2d(x1 - radius + Math.sin((double)i * Math.PI / 180.0) * radius, y1 - radius + Math.cos((double)i * Math.PI / 180.0) * radius);
+        }
+        for (i = 90; i <= 180; i += 3) {
+            GL11.glVertex2d(x1 - radius + Math.sin((double)i * Math.PI / 180.0) * radius, y + radius + Math.cos((double)i * Math.PI / 180.0) * radius);
+        }
+        GL11.glEnd();
+        GL11.glEnable(3553);
+        GL11.glDisable(3042);
+        GL11.glDisable(2848);
+        GL11.glDisable(3042);
+        GL11.glEnable(3553);
+        GL11.glScaled(2.0, 2.0, 2.0);
+        GL11.glPopAttrib();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
     }
 
     public static void drawRoundedRectBorder(float paramXStart, float paramYStart, float paramXEnd, float paramYEnd, float radius, int color, float width) {
