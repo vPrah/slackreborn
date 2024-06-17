f9e9d484e9512a1058b691bce0ba2a93ad65c04f
vPrah <165430229+vPrah@users.noreply.github.com>
2024-06-17 12:59:00 -0400

    DEV

--- src/main/java/cc/slack/Slack.java
+++ src/main/java/cc/slack/Slack.java
@@ -37,7 +37,7 @@ public class Slack {
 
     @Getter
     private static final Slack instance = new Slack();
-    public final ClientInfo info = new ClientInfo("Slack", "v2.0", ClientInfo.VersionType.BETA);
+    public final ClientInfo info = new ClientInfo("Slack", "v2.0", ClientInfo.VersionType.DEVELOPER);
     private final PubSub<Event> eventBus = PubSub.newInstance(System.err::println);
 
     private final ModuleManager moduleManager = new ModuleManager();
