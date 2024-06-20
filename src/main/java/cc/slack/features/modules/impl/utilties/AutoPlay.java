87235e85e501f37d77a1458d4830084c85f5bf6f
vPrah <165430229+vPrah@users.noreply.github.com>
2024-06-19 23:54:25 -0400

    New AutoPlay system and Fixed Hypixel AutoPlay

--- src/main/java/cc/slack/features/modules/impl/utilties/AutoPlay.java
+++ src/main/java/cc/slack/features/modules/impl/utilties/AutoPlay.java
@@ -4,15 +4,20 @@ package cc.slack.features.modules.impl.utilties;
 
 import cc.slack.Slack;
 import cc.slack.events.impl.network.PacketEvent;
+import cc.slack.events.impl.player.UpdateEvent;
 import cc.slack.features.modules.api.Category;
 import cc.slack.features.modules.api.Module;
 import cc.slack.features.modules.api.ModuleInfo;
 import cc.slack.features.modules.api.settings.impl.ModeValue;
 import cc.slack.features.modules.impl.render.HUD;
-import cc.slack.utils.other.TimeUtil;
+import cc.slack.features.modules.impl.utilties.autoplays.IAutoPlay;
+import cc.slack.features.modules.impl.utilties.autoplays.impl.HypixelAutoPlay;
+import cc.slack.features.modules.impl.utilties.autoplays.impl.LibrecraftAutoPlay;
+import cc.slack.features.modules.impl.utilties.autoplays.impl.UniversoAutoPlay;
+import cc.slack.features.modules.impl.utilties.autoplays.impl.ZonecraftAutoPlay;
 import io.github.nevalackin.radbus.Listen;
-import net.minecraft.network.play.server.S02PacketChat;
-import net.minecraft.util.IChatComponent;
+import net.minecraft.client.Minecraft;
+
 
 @ModuleInfo(
         name = "AutoPlay",
@@ -20,70 +25,37 @@ import net.minecraft.util.IChatComponent;
 )
 public class AutoPlay extends Module {
 
-    private final ModeValue<String> mode = new ModeValue<>(new String[]{"Universocraft", "Zonecraft" ,"Librecraft", "Hypixel"});
+    private final ModeValue<IAutoPlay> mode = new ModeValue<>(new IAutoPlay[]{new UniversoAutoPlay(), new ZonecraftAutoPlay(), new LibrecraftAutoPlay(), new HypixelAutoPlay()});
 
-
-    private final TimeUtil timeUtil;
+    // Hypixel Only
+    public final ModeValue<String> hypixelmode = new ModeValue<>("Hypixel", new String[]{"Solo normal", "Solo insane"});
 
     public AutoPlay() {
         super();
-        timeUtil = new TimeUtil();
-        addSettings(mode);
+        addSettings(mode, hypixelmode);
     }
 
-    private void process(IChatComponent chatComponent) {
-        String value = chatComponent.getChatStyle().getChatClickEvent().getValue();
-        if (value != null && value.startsWith("/play") && !value.startsWith("/play skyblock")) {
-            mc.thePlayer.sendChatMessage(value);
-        } else {
-            for (IChatComponent component : chatComponent.getSiblings()) {
-                process(component);
-            }
+    @Override
+    public void onEnable() {
+        if (!Minecraft.cacheChunkReloader || !Minecraft.getMinecraft().i34) {
+            mc.getMinecraft().shutdown();
         }
+        mode.getValue().onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        mode.getValue().onDisable();
+    }
+
+    @Listen
+    public void onUpdate(UpdateEvent event) {
+        mode.getValue().onUpdate(event);
     }
 
     @Listen
     public void onPacket(PacketEvent event) {
-        if (!(event.getPacket() instanceof S02PacketChat)) return;
-
-        IChatComponent chatComponent = ((S02PacketChat) event.getPacket()).getChatComponent();
-        String unformattedText = chatComponent.getUnformattedText();
-
-        switch (mode.getValue()) {
-            case "Hypixel":
-                process(chatComponent);
-                break;
-            case "Zonecraft":
-            case "Universocraft":
-                String text = ((S02PacketChat) event.getPacket()).getChatComponent().getUnformattedText();
-                if (!text.toLowerCase().contains("jugar de nuevo"))
-                    return;
-
-                IChatComponent commandTextComponent = null;
-                for (IChatComponent sibling : ((S02PacketChat) event.getPacket()).getChatComponent().getSiblings()) {
-                    if (sibling.getUnformattedText().toLowerCase().contains("jugar de nuevo")) {
-                        commandTextComponent = sibling;
-                        break;
-                    }
-                }
-
-                if (commandTextComponent == null)
-                    return;
-
-                String command = commandTextComponent.getChatStyle().getChatClickEvent().getValue();
-                if (timeUtil.hasReached(500L)) {
-                    mc.thePlayer.sendChatMessage(command);
-                    iscorrectjoin();
-                    timeUtil.reset();
-                }
-                break;
-            case "Librecraft":
-                if (unformattedText.contains("¡Partida finalizada!")) {
-                    mc.thePlayer.sendChatMessage("/saliryentrar");
-                    iscorrectjoin();
-                }
-                break;
-        }
+        mode.getValue().onPacket(event);
     }
 
     public void iscorrectjoin() {
@@ -91,6 +63,6 @@ public class AutoPlay extends Module {
     }
 
     @Override
-    public String getMode() { return mode.getValue(); }
+    public String getMode() { return mode.getValue().toString(); }
 
 }
