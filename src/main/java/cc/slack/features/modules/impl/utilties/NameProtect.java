308c10bcb5dda78eaf8f7dc37ca4b3073cbdd812
vPrah <165430229+vPrah@users.noreply.github.com>
2024-06-23 18:30:27 -0400

    NameProtect Recoded

--- src/main/java/cc/slack/features/modules/impl/utilties/NameProtect.java
+++ src/main/java/cc/slack/features/modules/impl/utilties/NameProtect.java
@@ -3,12 +3,15 @@ package cc.slack.features.modules.impl.utilties;
 import cc.slack.features.modules.api.Category;
 import cc.slack.features.modules.api.Module;
 import cc.slack.features.modules.api.ModuleInfo;
+import cc.slack.features.modules.api.settings.impl.StringValue;
 
 @ModuleInfo(
         name = "NameProtect",
         category = Category.UTILITIES
 )
 public class NameProtect extends Module {
+    public static final StringValue nametoprotect = new StringValue("Name", "§cSlack User"); // For new clickgui Display value
+
 
     public static String nameprotect;
 
@@ -17,7 +20,7 @@ public class NameProtect extends Module {
     }
 
     static {
-        NameProtect.nameprotect = "§cSlack User";
+        NameProtect.nameprotect = nametoprotect.getValue();
     }
 
 }
