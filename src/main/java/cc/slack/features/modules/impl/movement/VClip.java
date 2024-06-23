c46f44d605f5cb4874e5d46106a63700be79915b
vPrah <165430229+vPrah@users.noreply.github.com>
2024-06-23 17:49:18 -0400

    Recoded VClip

--- src/main/java/cc/slack/features/modules/impl/movement/VClip.java
+++ src/main/java/cc/slack/features/modules/impl/movement/VClip.java
@@ -4,6 +4,7 @@ import cc.slack.events.impl.player.UpdateEvent;
 import cc.slack.features.modules.api.Category;
 import cc.slack.features.modules.api.Module;
 import cc.slack.features.modules.api.ModuleInfo;
+import cc.slack.features.modules.api.settings.impl.ModeValue;
 import cc.slack.features.modules.api.settings.impl.NumberValue;
 import io.github.nevalackin.radbus.Listen;
 import net.minecraft.network.play.client.C03PacketPlayer;
@@ -14,16 +15,26 @@ import net.minecraft.network.play.client.C03PacketPlayer;
 )
 public class VClip extends Module {
 
-    private final NumberValue<Double> clipValue = new NumberValue<>("Down Value", 4.0D, 0.5D, 10D, 0.5D);
+    private final ModeValue<String> clipmodeValue = new ModeValue<>("Clip", new String[]{"Up", "Down"});
+    private final NumberValue<Double> clipValue = new NumberValue<>("Clip Value", 4.0D, 0.1D, 30.0D, 0.1D);
 
 
     public VClip() {
-        addSettings(clipValue);
+        addSettings(clipmodeValue, clipValue);
     }
 
+    @SuppressWarnings("unused")
     @Listen
     public void onUpdate (UpdateEvent event) {
-        mc.getNetHandler().addToSendQueue(new C03PacketPlayer.C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY - clipValue.getValue(), mc.thePlayer.posZ, true));
-        mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY - clipValue.getValue(), mc.thePlayer.posZ);
+        switch (clipmodeValue.getValue()) {
+            case "Up":
+                mc.getNetHandler().addToSendQueue(new C03PacketPlayer.C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY + clipValue.getValue(), mc.thePlayer.posZ, true));
+                mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY + clipValue.getValue(), mc.thePlayer.posZ);
+                break;
+            case "Down":
+                mc.getNetHandler().addToSendQueue(new C03PacketPlayer.C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY - clipValue.getValue(), mc.thePlayer.posZ, true));
+                mc.thePlayer.setPosition(mc.thePlayer.posX, mc.thePlayer.posY - clipValue.getValue(), mc.thePlayer.posZ);
+                break;
+        }
     }
 }
