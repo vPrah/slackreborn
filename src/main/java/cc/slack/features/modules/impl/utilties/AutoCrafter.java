6724593bc191358b3e2cff8f3e96128b88f2fa9b
vPrah <165430229+vPrah@users.noreply.github.com>
2024-07-09 21:39:40 -0400

    Updated AutoCrafter

--- src/main/java/cc/slack/features/modules/impl/utilties/AutoCrafter.java
+++ src/main/java/cc/slack/features/modules/impl/utilties/AutoCrafter.java
@@ -7,6 +7,7 @@ import cc.slack.features.modules.api.ModuleInfo;
 import cc.slack.features.modules.api.settings.impl.ModeValue;
 import cc.slack.utils.other.PrintUtil;
 import io.github.nevalackin.radbus.Listen;
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemBlock;
@@ -28,100 +29,79 @@ public class AutoCrafter extends Module {
         PrintUtil.message("Please, click on the crafting table.");
     }
 
+    @SuppressWarnings("unused")
     @Listen
-    public void onTick (TickEvent event) {
+    public void onTick(TickEvent event) {
+        if (!(mc.currentScreen instanceof net.minecraft.client.gui.inventory.GuiCrafting)) {
+            return;
+        }
+
+        int windowId = mc.thePlayer.openContainer.windowId;
+
         switch (crafterMode.getValue()) {
             case "Boat":
-                if (mc.currentScreen instanceof net.minecraft.client.gui.inventory.GuiCrafting) {
-                    int windowId = mc.thePlayer.openContainer.windowId;
-                    for (Slot inventorySlot : mc.thePlayer.inventoryContainer.inventorySlots) {
-                        if (inventorySlot.getHasStack() &&
-                                (inventorySlot.getStack()).stackSize >= 6 &&
-                                inventorySlot.getStack().getItem() instanceof ItemBlock && (
-                                (ItemBlock)inventorySlot.getStack().getItem()).getBlock() == Blocks.log) {
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber + 1, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 1, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 0, 0, 1, mc.thePlayer);
-                            return;
-                        }
-                    }
-                    for (Slot inventorySlot : mc.thePlayer.inventoryContainer.inventorySlots) {
-                        if (inventorySlot.getHasStack() &&
-                                (inventorySlot.getStack()).stackSize >= 6 &&
-                                inventorySlot.getStack().getItem() instanceof ItemBlock && (
-                                (ItemBlock)inventorySlot.getStack().getItem()).getBlock() == Blocks.planks) {
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber + 1, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 4, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 7, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 8, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 9, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 6, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber + 1, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 0, 0, 1, mc.thePlayer);
-                            for (Slot slot : mc.thePlayer.inventoryContainer.inventorySlots) {
-                                if (slot.getHasStack() &&
-                                        (slot.getStack()).stackSize >= 6 &&
-                                        slot.getStack().getItem() instanceof ItemBlock && (
-                                        (ItemBlock)slot.getStack().getItem()).getBlock() == Blocks.planks) {
-                                    mc.playerController.windowClick(windowId, slot.slotNumber + 1, 0, 0, mc.thePlayer);
-                                    mc.playerController.windowClick(windowId, 4, 1, 0, mc.thePlayer);
-                                    mc.playerController.windowClick(windowId, 7, 1, 0, mc.thePlayer);
-                                    mc.playerController.windowClick(windowId, 8, 1, 0, mc.thePlayer);
-                                    mc.playerController.windowClick(windowId, 9, 1, 0, mc.thePlayer);
-                                    mc.playerController.windowClick(windowId, 6, 1, 0, mc.thePlayer);
-                                    mc.playerController.windowClick(windowId, slot.slotNumber + 1, 0, 0, mc.thePlayer);
-                                    mc.playerController.windowClick(windowId, 0, 0, 1, mc.thePlayer);
-                                    mc.thePlayer.closeScreen();
-                                    return;
-                                }
-                            }
-                            mc.thePlayer.closeScreen();
-                            return;
-                        }
-                    }
-                }
+                handleBoatCrafting(windowId);
                 break;
             case "Furnace":
-                if (mc.currentScreen instanceof net.minecraft.client.gui.inventory.GuiCrafting) {
-                    int windowId = mc.thePlayer.openContainer.windowId;
-                    for (Slot inventorySlot : mc.thePlayer.inventoryContainer.inventorySlots) {
-                        if (inventorySlot.getHasStack() &&
-                                (inventorySlot.getStack()).stackSize >= 8 &&
-                                inventorySlot.getStack().getItem() instanceof ItemBlock && (
-                                (ItemBlock)inventorySlot.getStack().getItem()).getBlock() == Blocks.cobblestone) {
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 1, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 2, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 3, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 4, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 6, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 7, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 8, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, 9, 1, 0, mc.thePlayer);
-                            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
-                            
-                            mc.playerController.windowClick(windowId, 0, 0, 1, mc.thePlayer);
-                            mc.thePlayer.closeScreen();
-                            return;
-                        }
-                    }
-                }
+                handleFurnaceCrafting(windowId);
                 break;
         }
+    }
+
+    private void handleBoatCrafting(int windowId) {
+        for (Slot inventorySlot : mc.thePlayer.inventoryContainer.inventorySlots) {
+            if (isValidStack(inventorySlot, Blocks.log, 6)) {
+                mc.playerController.windowClick(windowId, inventorySlot.slotNumber + 1, 0, 0, mc.thePlayer);
+                mc.playerController.windowClick(windowId, 1, 0, 0, mc.thePlayer);
+                mc.playerController.windowClick(windowId, 0, 0, 1, mc.thePlayer);
+                return;
+            }
+        }
+
+        for (Slot inventorySlot : mc.thePlayer.inventoryContainer.inventorySlots) {
+            if (isValidStack(inventorySlot, Blocks.planks, 6)) {
+                clickBoatSlots(windowId, inventorySlot);
+                return;
+            }
+        }
+    }
+
+    private void handleFurnaceCrafting(int windowId) {
+        for (Slot inventorySlot : mc.thePlayer.inventoryContainer.inventorySlots) {
+            if (isValidStack(inventorySlot, Blocks.cobblestone, 8)) {
+                clickFurnaceSlots(windowId, inventorySlot);
+                return;
+            }
+        }
+    }
+
+    private void clickBoatSlots(int windowId, Slot inventorySlot) {
+        int[] slots = {4, 7, 8, 9, 6};
+        mc.playerController.windowClick(windowId, inventorySlot.slotNumber + 1, 0, 0, mc.thePlayer);
+        for (int slot : slots) {
+            mc.playerController.windowClick(windowId, slot, 1, 0, mc.thePlayer);
+        }
+        mc.playerController.windowClick(windowId, inventorySlot.slotNumber + 1, 0, 0, mc.thePlayer);
+        mc.playerController.windowClick(windowId, 0, 0, 1, mc.thePlayer);
+        mc.thePlayer.closeScreen();
+    }
+
+    private void clickFurnaceSlots(int windowId, Slot inventorySlot) {
+        int[] slots = {1, 2, 3, 4, 6, 7, 8, 9};
+        mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
+        for (int slot : slots) {
+            mc.playerController.windowClick(windowId, slot, 1, 0, mc.thePlayer);
+            mc.playerController.windowClick(windowId, inventorySlot.slotNumber, 0, 0, mc.thePlayer);
+        }
+        mc.playerController.windowClick(windowId, 0, 0, 1, mc.thePlayer);
+        mc.thePlayer.closeScreen();
+    }
+
+    private boolean isValidStack(Slot inventorySlot, Block block, int requiredStackSize) {
+        return inventorySlot.getHasStack() &&
+                inventorySlot.getStack().stackSize >= requiredStackSize &&
+                inventorySlot.getStack().getItem() instanceof ItemBlock &&
+                ((ItemBlock) inventorySlot.getStack().getItem()).getBlock() == block;
     }
 
 }
