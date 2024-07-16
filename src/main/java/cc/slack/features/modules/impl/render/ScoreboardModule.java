ad1d48ff6519aa25719d28e085cfb310332fe1dc
vPrah <165430229+vPrah@users.noreply.github.com>
2024-07-15 22:04:24 -0400

    Scoreboard custom font scale :sunglasses:

--- src/main/java/cc/slack/features/modules/impl/render/ScoreboardModule.java
+++ src/main/java/cc/slack/features/modules/impl/render/ScoreboardModule.java
@@ -9,10 +9,15 @@ import cc.slack.features.modules.api.Module;
 import cc.slack.features.modules.api.ModuleInfo;
 import cc.slack.features.modules.api.settings.impl.BooleanValue;
 import cc.slack.features.modules.api.settings.impl.ModeValue;
+import cc.slack.features.modules.api.settings.impl.NumberValue;
 import cc.slack.utils.drag.DragUtil;
 import cc.slack.utils.font.Fonts;
+import cc.slack.utils.font.MCFontRenderer;
 import cc.slack.utils.render.RenderUtil;
 import io.github.nevalackin.radbus.Listen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.scoreboard.Score;
 import net.minecraft.scoreboard.ScoreObjective;
@@ -28,11 +33,18 @@ public class ScoreboardModule extends Module {
 	private final BooleanValue noscoreboard = new BooleanValue("No Scoreboard", false);
 	private final BooleanValue roundedValue = new BooleanValue("Rounded", false);
 	private final BooleanValue textShadow = new BooleanValue("Text Shadow", false);
-	private final ModeValue<String> scoreboardFont = new ModeValue<>("Scoreboard Font", new String[]{"Minecraft", "Apple", "Poppins", "Roboto"});
-	private final ModeValue<String> scoreboardFontScale = new ModeValue<>("Font Scale", new String[]{"18","20", "24"});
+	private final ModeValue<String> scoreboardFont = new ModeValue<>("Font", new String[]{"Minecraft", "Apple", "Poppins", "Roboto"});
+	private static final NumberValue<Integer> scoreboardFontScale = new NumberValue<>("Font Scale", 18, 1, 30, 1);
+//	private final ModeValue<String> scoreboardFontScale = new ModeValue<>("Font Scale", new String[]{"18","20", "24"});
 
 	double posX = 0.0D;
 	double posY = 30.0D;
+
+	private MCFontRenderer appleFontRenderer;
+	private MCFontRenderer poppinsFontRenderer;
+	private MCFontRenderer robotoFontRenderer;
+
+	private int lastFontScaleValue = -1;
 
 
 	public ScoreboardModule() {
@@ -44,10 +56,22 @@ public class ScoreboardModule extends Module {
 		event.cancel();
 	}
 
+	private void updateFontRenderers() {
+		int currentScaleValue = scoreboardFontScale.getValue();
+		if (currentScaleValue != lastFontScaleValue) {
+			appleFontRenderer = Fonts.getFontRenderer("Apple", currentScaleValue);
+			poppinsFontRenderer = Fonts.getFontRenderer("Poppins", currentScaleValue);
+			robotoFontRenderer = Fonts.getFontRenderer("Roboto", currentScaleValue);
+			lastFontScaleValue = currentScaleValue;
+		}
+	}
+
 	@Listen
 	public void onRender(RenderEvent event) {
 		if (event.getState() != RenderEvent.State.RENDER_2D) return;
 		if (noscoreboard.getValue()) return;
+
+		updateFontRenderers(); // Actualiza las fuentes si es necesario
 
 		ScaledResolution scaledRes = new ScaledResolution(mc);
 		Scoreboard scoreboard = mc.theWorld.getScoreboard();
@@ -78,108 +102,56 @@ public class ScoreboardModule extends Module {
 		if (roundedValue.getValue()) {
 			RenderUtil.drawRoundedRect((float) pos[0], (float) pos[1], (float) (pos[0] + width), (float) (pos[1] + height), 8F, 1342177280);
 		} else {
-			drawRect(pos[0], pos[1] + 2 , pos[0] + width, pos[1] + 12, 1610612736); // top
+			drawRect(pos[0], pos[1] + 2, pos[0] + width, pos[1] + 12, 1610612736); // top
 			drawRect(pos[0], pos[1] + 2, (pos[0] + width), pos[1] + height, 1342177280);
 		}
 
 		switch (scoreboardFont.getValue()) {
 			case "Minecraft":
-				mc.MCfontRenderer.drawString(objective.getDisplayName(), (float) ((float) pos[0] + width / 2 - mc.MCfontRenderer.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
+				mc.MCfontRenderer.drawString(objective.getDisplayName(), (float) (pos[0] + width / 2 - mc.MCfontRenderer.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
 				break;
 			case "Apple":
-				switch (scoreboardFontScale.getValue()) {
-					case "18":
-						Fonts.apple18.drawString(objective.getDisplayName(), (float) ((float) pos[0] + width / 2 - Fonts.apple18.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
-						break;
-					case "20":
-						Fonts.apple20.drawString(objective.getDisplayName(), (float) ((float) pos[0] + width / 2 - Fonts.apple20.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
-						break;
-					case "24":
-						Fonts.apple24.drawString(objective.getDisplayName(), (float) ((float) pos[0] + width / 2 - Fonts.apple24.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
-						break;
-				}
+				appleFontRenderer.drawString(objective.getDisplayName(), (float) (pos[0] + width / 2 - appleFontRenderer.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
 				break;
 			case "Poppins":
-				switch (scoreboardFontScale.getValue()) {
-					case "18":
-						Fonts.poppins18.drawString(objective.getDisplayName(), (float) ((float) pos[0] + width / 2 - Fonts.poppins18.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
-						break;
-					case "20":
-						Fonts.poppins20.drawString(objective.getDisplayName(), (float) ((float) pos[0] + width / 2 - Fonts.poppins20.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
-						break;
-					case "24":
-						Fonts.poppins24.drawString(objective.getDisplayName(), (float) ((float) pos[0] + width / 2 - Fonts.poppins24.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
-						break;
-				}
+				poppinsFontRenderer.drawString(objective.getDisplayName(), (float) (pos[0] + width / 2 - poppinsFontRenderer.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
 				break;
 			case "Roboto":
-				switch (scoreboardFontScale.getValue()) {
-					case "18":
-						Fonts.roboto18.drawString(objective.getDisplayName(), (float) ((float) pos[0] + width / 2 - Fonts.roboto18.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
-						break;
-					case "20":
-						Fonts.roboto20.drawString(objective.getDisplayName(), (float) ((float) pos[0] + width / 2 - Fonts.roboto20.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
-						break;
-					case "24":
-						Fonts.roboto24.drawString(objective.getDisplayName(), (float) ((float) pos[0] + width / 2 - Fonts.roboto24.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
-						break;
-				}
+				robotoFontRenderer.drawString(objective.getDisplayName(), (float) (pos[0] + width / 2 - robotoFontRenderer.getStringWidth(objective.getDisplayName()) / 2), (float) pos[1] + 4, -1, textShadow.getValue());
 				break;
 		}
+
 		int j = 0;
 		for (Score score1 : collection) {
 			++j;
 			ScorePlayerTeam scoreplayerteam1 = scoreboard.getPlayersTeam(score1.getPlayerName());
 			String s1 = ScorePlayerTeam.formatPlayerName(scoreplayerteam1, score1.getPlayerName());
 			String s2 = ChatFormatting.RED + "" + score1.getScorePoints();
+
 			switch (scoreboardFont.getValue()) {
 				case "Minecraft":
-					mc.MCfontRenderer.drawString(s1, (float) pos[0] + 3, (float) ((float) pos[1] + height - 9.2 * j), -1, textShadow.getValue());
+					mc.MCfontRenderer.drawString(s1, (float) pos[0] + 3, (float) (pos[1] + height - 9.2 * j), -1, textShadow.getValue());
 					mc.MCfontRenderer.drawString(s2, (float) (pos[0] + width - mc.MCfontRenderer.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());
 					break;
 				case "Apple":
-					switch (scoreboardFontScale.getValue()) {
-						case "18":
-							Fonts.apple18.drawString(s1, (float) pos[0] + 3, (float) ((float) pos[1] + height - 9.2 * j), -1, textShadow.getValue());
-							Fonts.apple18.drawString(s2, (float) (pos[0] + width - Fonts.apple18.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());							break;
-						case "20":
-							Fonts.apple20.drawString(s1, (float) pos[0] + 3, (float) ((float) pos[1] + height - 9.2 * j), -1, textShadow.getValue());
-							Fonts.apple20.drawString(s2, (float) (pos[0] + width - Fonts.apple20.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());							break;
-						case "24":
-							Fonts.apple24.drawString(s1, (float) pos[0] + 3, (float) ((float) pos[1] + height - 9.2 * j), -1, textShadow.getValue());
-							Fonts.apple24.drawString(s2, (float) (pos[0] + width - Fonts.apple24.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());							break;
-					}
+					appleFontRenderer.drawString(s1, (float) pos[0] + 3, (float) (pos[1] + height - 9.2 * j), -1, textShadow.getValue());
+					appleFontRenderer.drawString(s2, (float) (pos[0] + width - appleFontRenderer.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());
 					break;
 				case "Poppins":
-					switch (scoreboardFontScale.getValue()) {
-						case "18":
-							Fonts.poppins18.drawString(s1, (float) pos[0] + 3, (float) ((float) pos[1] + height - 9.2 * j), -1, textShadow.getValue());
-							Fonts.poppins18.drawString(s2, (float) (pos[0] + width - Fonts.poppins18.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());							break;
-						case "20":
-							Fonts.poppins20.drawString(s1, (float) pos[0] + 3, (float) ((float) pos[1] + height - 9.2 * j), -1, textShadow.getValue());
-							Fonts.poppins20.drawString(s2, (float) (pos[0] + width - Fonts.poppins20.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());							break;
-						case "24":
-							Fonts.poppins24.drawString(s1, (float) pos[0] + 3, (float) ((float) pos[1] + height - 9.2 * j), -1, textShadow.getValue());
-							Fonts.poppins24.drawString(s2, (float) (pos[0] + width - Fonts.poppins24.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());							break;
-					}
+					poppinsFontRenderer.drawString(s1, (float) pos[0] + 3, (float) (pos[1] + height - 9.2 * j), -1, textShadow.getValue());
+					poppinsFontRenderer.drawString(s2, (float) (pos[0] + width - poppinsFontRenderer.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());
 					break;
 				case "Roboto":
-					switch (scoreboardFontScale.getValue()) {
-						case "18":
-							Fonts.roboto18.drawString(s1, (float) pos[0] + 3, (float) ((float) pos[1] + height - 9.2 * j), -1, textShadow.getValue());
-							Fonts.roboto18.drawString(s2, (float) (pos[0] + width - Fonts.roboto18.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());							break;
-						case "20":
-							Fonts.roboto20.drawString(s1, (float) pos[0] + 3, (float) ((float) pos[1] + height - 9.2 * j), -1, textShadow.getValue());
-							Fonts.roboto20.drawString(s2, (float) (pos[0] + width - Fonts.roboto20.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());							break;
-						case "24":
-							Fonts.roboto24.drawString(s1, (float) pos[0] + 3, (float) ((float) pos[1] + height - 9.2 * j), -1, textShadow.getValue());
-							Fonts.roboto24.drawString(s2, (float) (pos[0] + width - Fonts.roboto24.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());							break;
-					}
+					robotoFontRenderer.drawString(s1, (float) pos[0] + 3, (float) (pos[1] + height - 9.2 * j), -1, textShadow.getValue());
+					robotoFontRenderer.drawString(s2, (float) (pos[0] + width - robotoFontRenderer.getStringWidth(s2)), (float) (pos[1] + height - 9 * j), -1, textShadow.getValue());
 					break;
 			}
 		}
 	}
-	
+
+
+
+
 
 	@Override
 	public DragUtil getPosition() {
