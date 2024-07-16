fc9ee8c54cacfc524441fa08c2c520f039dcc51d
vPrah <165430229+vPrah@users.noreply.github.com>
2024-07-15 22:03:53 -0400

    Font Utils recode

--- src/main/java/cc/slack/utils/font/Fonts.java
+++ src/main/java/cc/slack/utils/font/Fonts.java
@@ -4,61 +4,101 @@ import cc.slack.Slack;
 import cc.slack.utils.other.PrintUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.ResourceLocation;
+import sun.font.FontManager;
 
 import java.awt.*;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Fonts {
-    public static final MCFontRenderer IconFont = new MCFontRenderer(fontFromTTF("guiicons.ttf", 24), true, true);
-    public static final MCFontRenderer IconFontBig = new MCFontRenderer(fontFromTTF("guiicons.ttf", 38), true, true);
+    private static final Map<String, Font> loadedFonts = new HashMap<>();
+    private static final Map<String, String> fontFiles = new HashMap<>();
 
-    public static final MCFontRenderer SFBold30 = new MCFontRenderer(fontFromTTF("sfsemibold.ttf", 30), true, true);
-    public static final MCFontRenderer SFBold18 = new MCFontRenderer(fontFromTTF("sfsemibold.ttf", 18), true, true);
-    public static final MCFontRenderer SFReg18 = new MCFontRenderer(fontFromTTF("sfregular.ttf", 18), true, true);
-    public static final MCFontRenderer SFReg24 = new MCFontRenderer(fontFromTTF("sfregular.ttf", 24), true, true);
-    public static final MCFontRenderer SFReg45 = new MCFontRenderer(fontFromTTF("sfregular.ttf", 45), true, true);
-    public static final MCFontRenderer axi24 = new MCFontRenderer(fontFromTTF("axi.ttf", 24), true, false);
-    public static final MCFontRenderer axi12 = new MCFontRenderer(fontFromTTF("axi.ttf", 12), true, false);
-    public static final MCFontRenderer axi16 = new MCFontRenderer(fontFromTTF("axi.ttf", 16), true, false);
-    public static final MCFontRenderer axi18 = new MCFontRenderer(fontFromTTF("axi.ttf", 18), true, false);
-    public static final MCFontRenderer axi35 = new MCFontRenderer(fontFromTTF("axi.ttf", 35), true, false);
-    public static final MCFontRenderer axi45 = new MCFontRenderer(fontFromTTF("axi.ttf", 45), true, false);
-    // Poppins
-    public static final MCFontRenderer poppins10 = new MCFontRenderer(fontFromTTF("poppins-Light.ttf", 10), true, false);
-    public static final MCFontRenderer poppins18 = new MCFontRenderer(fontFromTTF("poppins-Light.ttf", 18), true, false);
-    public static final MCFontRenderer poppins20 = new MCFontRenderer(fontFromTTF("poppins-Light.ttf", 20), true, false);
-    public static final MCFontRenderer poppins24 = new MCFontRenderer(fontFromTTF("poppins-Light.ttf", 24), true, false);
-
-    // Apple
-    public static final MCFontRenderer apple18 = new MCFontRenderer(fontFromTTF("apple.ttf", 18), true, false);
-    public static final MCFontRenderer apple20 = new MCFontRenderer(fontFromTTF("apple.ttf", 20), true, false);
-    public static final MCFontRenderer apple24 = new MCFontRenderer(fontFromTTF("apple.ttf", 24), true, false);
-    public static final MCFontRenderer apple45 = new MCFontRenderer(fontFromTTF("apple.ttf", 45), true, true);
-
-    // Roboto
-    public static final MCFontRenderer roboto10 = new MCFontRenderer(fontFromTTF("roboto.ttf", 10), true, false);
-    public static final MCFontRenderer roboto18 = new MCFontRenderer(fontFromTTF("roboto.ttf", 18), true, false);
-    public static final MCFontRenderer roboto20 = new MCFontRenderer(fontFromTTF("roboto.ttf", 20), true, false);
-    public static final MCFontRenderer roboto24 = new MCFontRenderer(fontFromTTF("roboto.ttf", 24), true, false);
-
-    public static final MCFontRenderer Arial18 = new MCFontRenderer(new Font("Arial", Font.PLAIN, 18), true, true);
-    public static final MCFontRenderer Arial45 = new MCFontRenderer(new Font("Arial", Font.PLAIN, 45), true, false);
-    public static final MCFontRenderer Arial65 = new MCFontRenderer(new Font("Arial", Font.PLAIN, 65), true, false);
-    public static final MCFontRenderer Checkmark = new MCFontRenderer(fontFromTTF("checkmark.ttf", 24), true, false);
-
-    private static Font fontFromTTF(String fileName, float fontSize) {
-        if (Slack.getInstance().info.type == cc.slack.utils.client.ClientInfo.VersionType.DEVELOPER) {
-            PrintUtil.print("Initializing Font: " + fileName + " | Size: " + fontSize);
-        }
-        Font output = null;
-        try {
-            output = Font.createFont(Font.TRUETYPE_FONT, Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation("slack/fonts/" + fileName)).getInputStream());
-            output = output.deriveFont(fontSize);
-        } catch (FontFormatException | IOException e) {
-            // No More
-        }
-        return output;
+    static {
+        // Registrar las fuentes con su archivo correspondiente
+        fontFiles.put("Poppins", "poppins-Light.ttf");
+        fontFiles.put("IconFont", "guiicons.ttf");
+        fontFiles.put("SFBold", "sfsemibold.ttf");
+        fontFiles.put("SFReg", "sfregular.ttf");
+        fontFiles.put("Axi", "axi.ttf");
+        fontFiles.put("Apple", "apple.ttf");
+        fontFiles.put("Roboto", "roboto.ttf");
+        fontFiles.put("Arial", "Arial"); // Asume que Arial es una fuente del sistema
+        fontFiles.put("Checkmark", "checkmark.ttf");
     }
+
+    public static MCFontRenderer getFontRenderer(String fontName, int fontSize, boolean antiAlias, boolean fractionalMetrics) {
+        Font baseFont = getBaseFont(fontName);
+        Font sizedFont = baseFont.deriveFont((float) fontSize);
+        return new MCFontRenderer(sizedFont, antiAlias, fractionalMetrics);
+    }
+
+    public static MCFontRenderer getFontRenderer(String fontName, int fontSize) {
+        return getFontRenderer(fontName, fontSize, true, false);
+    }
+
+    private static Font getBaseFont(String fontName) {
+        if (!loadedFonts.containsKey(fontName)) {
+            String fontFile = fontFiles.get(fontName);
+            Font font = loadFont(fontFile);
+            loadedFonts.put(fontName, font);
+        }
+        return loadedFonts.get(fontName);
+    }
+
+    private static Font loadFont(String fontFileName) {
+        try {
+            if (fontFileName.equals("Arial")) {
+                return new Font("Arial", Font.PLAIN, 24); // Tamaño arbitrario, se cambiará más adelante
+            }
+            InputStream is = Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation("slack/fonts/" + fontFileName)).getInputStream();
+            return Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (FontFormatException | IOException e) {
+            e.printStackTrace();
+            return new Font("Arial", Font.PLAIN, 24); // Fallback a Arial si hay un error
+        }
+    }
+
+    // Ejemplo de uso
+    public static final MCFontRenderer poppins10 = getFontRenderer("Poppins", 10);
+    public static final MCFontRenderer poppins18 = getFontRenderer("Poppins", 18);
+    public static final MCFontRenderer poppins20 = getFontRenderer("Poppins", 20);
+    public static final MCFontRenderer poppins24 = getFontRenderer("Poppins", 24);
+
+    public static final MCFontRenderer iconFont24 = getFontRenderer("IconFont", 24, true, true);
+    public static final MCFontRenderer iconFont38 = getFontRenderer("IconFont", 38, true, true);
+
+    public static final MCFontRenderer sfBold30 = getFontRenderer("SFBold", 30, true, true);
+    public static final MCFontRenderer sfBold18 = getFontRenderer("SFBold", 18, true, true);
+    public static final MCFontRenderer sfReg18 = getFontRenderer("SFReg", 18, true, true);
+    public static final MCFontRenderer sfReg24 = getFontRenderer("SFReg", 24, true, true);
+    public static final MCFontRenderer sfReg45 = getFontRenderer("SFReg", 45, true, true);
+
+    public static final MCFontRenderer axi24 = getFontRenderer("Axi", 24);
+    public static final MCFontRenderer axi12 = getFontRenderer("Axi", 12);
+    public static final MCFontRenderer axi16 = getFontRenderer("Axi", 16);
+    public static final MCFontRenderer axi18 = getFontRenderer("Axi", 18);
+    public static final MCFontRenderer axi35 = getFontRenderer("Axi", 35);
+    public static final MCFontRenderer axi45 = getFontRenderer("Axi", 45);
+
+    public static final MCFontRenderer apple18 = getFontRenderer("Apple", 18);
+    public static final MCFontRenderer apple20 = getFontRenderer("Apple", 20);
+    public static final MCFontRenderer apple24 = getFontRenderer("Apple", 24);
+    public static final MCFontRenderer apple45 = getFontRenderer("Apple", 45, true, true);
+
+    public static final MCFontRenderer roboto10 = getFontRenderer("Roboto", 10);
+    public static final MCFontRenderer roboto18 = getFontRenderer("Roboto", 18);
+    public static final MCFontRenderer roboto20 = getFontRenderer("Roboto", 20);
+    public static final MCFontRenderer roboto24 = getFontRenderer("Roboto", 24);
+
+    public static final MCFontRenderer arial18 = getFontRenderer("Arial", 18, true, true);
+    public static final MCFontRenderer arial45 = getFontRenderer("Arial", 45, true, false);
+    public static final MCFontRenderer arial65 = getFontRenderer("Arial", 65, true, false);
+
+    public static final MCFontRenderer checkmark24 = getFontRenderer("Checkmark", 24);
+
 
 }
