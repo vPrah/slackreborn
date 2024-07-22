e74b931534d1691575fc5b358f8d501cc0434a47
vPrah <165430229+vPrah@users.noreply.github.com>
2024-07-22 19:40:12 -0400

    VerusSpider fixed

--- src/main/java/cc/slack/features/modules/impl/movement/spiders/impl/VerusSpider.java
+++ src/main/java/cc/slack/features/modules/impl/movement/spiders/impl/VerusSpider.java
@@ -1,6 +1,27 @@
 package cc.slack.features.modules.impl.movement.spiders.impl;
 
+import cc.slack.events.impl.player.MotionEvent;
 import cc.slack.features.modules.impl.movement.spiders.ISpider;
+import cc.slack.utils.player.PlayerUtil;
+import net.minecraft.block.BlockAir;
 
 public class VerusSpider implements ISpider {
+
+    @Override
+    public void onMotion(MotionEvent event) {
+        if (!mc.thePlayer.onGround && !mc.gameSettings.keyBindSneak.isKeyDown()) {
+            if (mc.thePlayer.posY + mc.thePlayer.motionY < Math.floor(mc.thePlayer.posY))
+                if (!(PlayerUtil.getBlockRelativeToPlayer(-1, -1, 0) instanceof BlockAir) || !(PlayerUtil.getBlockRelativeToPlayer(1, -1, 0) instanceof BlockAir) || !(PlayerUtil.getBlockRelativeToPlayer(0, -1, -1) instanceof BlockAir) || !(PlayerUtil.getBlockRelativeToPlayer(0, -1, 1) instanceof BlockAir))
+                    mc.thePlayer.motionY = (Math.floor(mc.thePlayer.posY) - (mc.thePlayer.posY));
+            if (mc.thePlayer.motionY == 0) {
+                mc.thePlayer.onGround = true;
+                event.setGround(true);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Verus";
+    }
 }
