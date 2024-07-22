2eee52d541269c75a99d4b15cf9c336cda2251d5
vPrah <165430229+vPrah@users.noreply.github.com>
2024-07-22 19:44:35 -0400

    Added VanillaFlight fly speed values

--- src/main/java/cc/slack/features/modules/impl/movement/flights/impl/vanilla/VanillaFlight.java
+++ src/main/java/cc/slack/features/modules/impl/movement/flights/impl/vanilla/VanillaFlight.java
@@ -2,7 +2,9 @@
 
 package cc.slack.features.modules.impl.movement.flights.impl.vanilla;
 
+import cc.slack.Slack;
 import cc.slack.events.impl.player.MoveEvent;
+import cc.slack.features.modules.impl.movement.Flight;
 import cc.slack.features.modules.impl.movement.flights.IFlight;
 import cc.slack.utils.player.MovementUtil;
 
@@ -17,7 +19,7 @@ public class VanillaFlight implements IFlight {
     public void onMove(MoveEvent event) {
         event.setY((mc.gameSettings.keyBindJump.isKeyDown() ? 1 * 3.32 :
                 mc.gameSettings.keyBindSneak.isKeyDown() ? -1 * 3.32 : 0));
-        MovementUtil.setSpeed(event, 3.5);
+        MovementUtil.setSpeed(event, Slack.getInstance().getModuleManager().getInstance(Flight.class).vanillaspeed.getValue());
     }
 
     @Override
