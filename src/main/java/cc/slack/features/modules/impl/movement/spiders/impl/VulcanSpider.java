8649d2221661fcbd1792e3d2cfdedaeac29d6e0d
vPrah <165430229+vPrah@users.noreply.github.com>
2024-08-03 19:13:41 -0400

    Updated and improved the VulcanSpider

--- src/main/java/cc/slack/features/modules/impl/movement/spiders/impl/VulcanSpider.java
+++ src/main/java/cc/slack/features/modules/impl/movement/spiders/impl/VulcanSpider.java
@@ -1,59 +1,31 @@
 package cc.slack.features.modules.impl.movement.spiders.impl;
 
+import cc.slack.events.State;
 import cc.slack.events.impl.network.PacketEvent;
 import cc.slack.events.impl.player.MotionEvent;
 import cc.slack.features.modules.impl.movement.spiders.ISpider;
 import cc.slack.utils.player.MovementUtil;
+import cc.slack.utils.player.PlayerUtil;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.client.C03PacketPlayer;
 import net.minecraft.util.MathHelper;
 
 public class VulcanSpider implements ISpider {
 
-    private int vulcanTicks;
-
     @Override
     public void onMotion(MotionEvent event) {
-        if (mc.thePlayer.isCollidedHorizontally) {
-            if (mc.thePlayer.onGround) {
-                vulcanTicks = 0;
-                mc.thePlayer.jump();
-            }
-            if (vulcanTicks >= 3) {
-                vulcanTicks = 0;
-            }
-            vulcanTicks++;
-            switch (vulcanTicks) {
-                case 2:
-                case 3:
-                    mc.thePlayer.jump();
-                    MovementUtil.resetMotion(false);
-            }
-        }
-    }
-
-    @Override
-    public void onPacket(PacketEvent event) {
-        Packet packet = event.getPacket();
-        if (packet instanceof C03PacketPlayer) {
+        if (event.getState() == State.PRE) {
             if (mc.thePlayer.isCollidedHorizontally) {
-                switch (vulcanTicks) {
-                    case 2:
-                        ((C03PacketPlayer) packet).onGround = true;
-                        break;
-                    case 3:
-                        ((C03PacketPlayer) packet).y -= 0.05;
-                        ((C03PacketPlayer) packet).x -= MathHelper.sin((float) Math.toRadians(mc.thePlayer.rotationYaw)) * 0.1;
-                        ((C03PacketPlayer) packet).z += MathHelper.cos((float) Math.toRadians(mc.thePlayer.rotationYaw)) * 0.1;
+                if (mc.thePlayer.ticksExisted % 2 == 0) {
+                    event.setGround(true);
+                    mc.thePlayer.motionY = PlayerUtil.getJumpHeight();
                 }
-                event.setPacket(packet);
+
+                final double yaw = MovementUtil.getDirection();
+                event.setX(event.getX() - -MathHelper.sin((float) yaw) * 0.1f);
+                event.setZ(event.getZ() - MathHelper.cos((float) yaw) * 0.1f);
             }
         }
-    }
-
-    @Override
-    public void onDisable() {
-        vulcanTicks = 0;
     }
 
     @Override
