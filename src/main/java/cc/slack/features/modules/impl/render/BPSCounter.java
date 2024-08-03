20422b15db6b300c1de9f779bada6942284b1cf2
vPrah <165430229+vPrah@users.noreply.github.com>
2024-08-03 18:53:30 -0400

    Added BPSCounter Module (you can drag it and custom)

---
+++ src/main/java/cc/slack/features/modules/impl/render/BPSCounter.java
@@ -0,0 +1,126 @@
+package cc.slack.features.modules.impl.render;
+
+import cc.slack.events.impl.render.RenderEvent;
+import cc.slack.features.modules.api.Category;
+import cc.slack.features.modules.api.Module;
+import cc.slack.features.modules.api.ModuleInfo;
+import cc.slack.features.modules.api.settings.impl.BooleanValue;
+import cc.slack.features.modules.api.settings.impl.NumberValue;
+import cc.slack.start.Slack;
+import cc.slack.utils.drag.DragUtil;
+import cc.slack.utils.font.Fonts;
+import cc.slack.utils.player.MovementUtil;
+import cc.slack.utils.render.ColorUtil;
+import cc.slack.utils.render.RenderUtil;
+import io.github.nevalackin.radbus.Listen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.ScaledResolution;
+import org.lwjgl.input.Mouse;
+
+import java.awt.*;
+
+import static java.lang.Math.round;
+
+@ModuleInfo(
+        name = "BPSCounter",
+        category = Category.RENDER
+)
+public class BPSCounter extends Module {
+    private final BooleanValue rounded = new BooleanValue("Rounded", true);
+    private final NumberValue<Integer> widthValue = new NumberValue<>("Background Width", 50, 10, 100, 1);
+    private final NumberValue<Integer> heightValue = new NumberValue<>("Background Height", 18, 5, 100, 1);
+    private final NumberValue<Integer> radiusValue = new NumberValue<>("Background Rounded Radius", 6, 0 , 20, 1);
+
+
+    private double posX = -1D;
+    private double posY = -1D;
+    private int x = 0;
+    private int y = 0;
+
+    private boolean dragging = false;
+    private double dragX = 0, dragY = 0;
+
+    public BPSCounter () {
+        addSettings(rounded, widthValue, heightValue, radiusValue);
+    }
+
+    @SuppressWarnings("unused")
+    @Listen
+    public void onRender(RenderEvent event) {
+        if (mc.gameSettings.showDebugInfo) {
+            return;
+        }
+
+        ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
+        if (posX == -1 || posY == -1) {
+            posX = sr.getScaledWidth() / 2.0 + 90;
+            posY = sr.getScaledHeight() / 2.0 - 20;
+        }
+
+        int mouseX = Mouse.getX() * sr.getScaledWidth() / mc.displayWidth;
+        int mouseY = sr.getScaledHeight() - Mouse.getY() * sr.getScaledHeight() / mc.displayHeight - 1;
+
+        if (dragging) {
+            posX = mouseX - dragX;
+            posY = mouseY - dragY;
+        }
+
+        x = (int) posX;
+        y = (int) posY;
+
+        int rectX = x + 170;
+        int rectY = y + 58;
+        int rectWidth = widthValue.getValue();
+        int rectHeight = heightValue.getValue();
+        int cornerRadius = radiusValue.getValue();
+
+        if (rounded.getValue()) {
+            drawRoundedRect(rectX, rectY, rectWidth, rectHeight, cornerRadius, new Color(0, 0, 0, 150).getRGB());
+        } else {
+            drawRect(rectX, rectY, rectWidth, rectHeight, new Color(0, 0, 0, 150).getRGB());
+        }
+
+        String fpsText = "BPS: ";
+        String fpsValue = "" + getBPS();
+
+        int fpsTextWidth = Fonts.apple20.getStringWidth(fpsText);
+        int fpsValueWidth = Fonts.apple20.getStringWidth(fpsValue);
+        int totalTextWidth = fpsTextWidth + fpsValueWidth;
+
+        int textX = rectX + (rectWidth - totalTextWidth) / 2;
+        int textY = rectY + (rectHeight - Fonts.apple20.getHeight()) / 2;
+
+        Fonts.apple20.drawStringWithShadow(fpsText, textX, textY, ColorUtil.getColor(Slack.getInstance().getModuleManager().getInstance(HUD.class).theme.getValue(), 0.15).getRGB());
+        Fonts.apple20.drawStringWithShadow(fpsValue, textX + fpsTextWidth, textY, -1);
+
+        handleMouseInput(mouseX, mouseY, rectX, rectY, rectWidth, rectHeight);
+    }
+
+    private void handleMouseInput(int mouseX, int mouseY, int rectX, int rectY, int rectWidth, int rectHeight) {
+        if (Mouse.isButtonDown(0)) {
+            if (!dragging) {
+                if (mouseX >= rectX && mouseX <= rectX + rectWidth &&
+                        mouseY >= rectY && mouseY <= rectY + rectHeight) {
+                    dragging = true;
+                    dragX = mouseX - posX;
+                    dragY = mouseY - posY;
+                }
+            }
+        } else {
+            dragging = false;
+        }
+    }
+    private void drawRoundedRect(float x, float y, float width, float height, float radius, int color) {
+        RenderUtil.drawRoundedRect(x, y, x + width, y + height, radius, color);
+    }
+
+    private void drawRect(int x, int y, int width, int height, int color) {
+        Gui.drawRect(x, y, x + width, y + height, color);
+    }
+
+    private String getBPS() {
+        double currentBPS = ((double) round((MovementUtil.getSpeed() * 20) * 100)) / 100;
+        return String.format("%.2f", currentBPS);
+    }
+}
