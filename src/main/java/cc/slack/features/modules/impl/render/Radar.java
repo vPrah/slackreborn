7dfd54ad4a9a9a326f2d237d470c9a55f072e409
vPrah <165430229+vPrah@users.noreply.github.com>
2024-08-04 16:29:26 -0400

    Radar supports "ResetPosition"

--- src/main/java/cc/slack/features/modules/impl/render/Radar.java
+++ src/main/java/cc/slack/features/modules/impl/render/Radar.java
@@ -7,6 +7,7 @@ import cc.slack.features.modules.api.ModuleInfo;
 import cc.slack.features.modules.api.settings.impl.BooleanValue;
 import cc.slack.features.modules.api.settings.impl.NumberValue;
 
+import cc.slack.start.Slack;
 import cc.slack.utils.render.RenderUtil;
 import io.github.nevalackin.radbus.Listen;
 import net.minecraft.client.Minecraft;
@@ -24,20 +25,29 @@ public class Radar extends Module {
     private final NumberValue<Float> yValue = new NumberValue<>("Pos Y", 178F, 1.0F, 900.0F, 1F);
     private final NumberValue<Float> scaleValue = new NumberValue<>("Scale", 11.5F, 1.0F, 30.0F, 0.1F);
     private final BooleanValue roundedValue = new BooleanValue("Rounded", false);
+    public final BooleanValue resetPos = new BooleanValue("Reset Position", false);
+
 
     private boolean dragging = false;
     private double dragX = 0, dragY = 0;
 
     public Radar() {
-        addSettings(xValue, yValue, scaleValue, roundedValue);
+        addSettings(xValue, yValue, scaleValue, roundedValue, resetPos);
     }
 
     @Listen
     public void onRender(RenderEvent event) {
+        if (resetPos.getValue()) {
+            xValue.setValue(8F);
+            yValue.setValue(170F);
+            Slack.getInstance().getModuleManager().getInstance(Radar.class).resetPos.setValue(false);
+        }
+
         float x = xValue.getValue();
         float y = yValue.getValue();
         float scale = scaleValue.getValue();
         boolean rounded = roundedValue.getValue();
+
 
         ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
         int mouseX = Mouse.getX() * sr.getScaledWidth() / mc.displayWidth;
