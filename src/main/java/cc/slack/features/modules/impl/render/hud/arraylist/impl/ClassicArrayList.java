078e60c15f5b9aab15ef230085880be773aa4fa4
vPrah <165430229+vPrah@users.noreply.github.com>
2024-08-04 15:53:01 -0400

    ClassicArrayList now supports Drag

--- src/main/java/cc/slack/features/modules/impl/render/hud/arraylist/impl/ClassicArrayList.java
+++ src/main/java/cc/slack/features/modules/impl/render/hud/arraylist/impl/ClassicArrayList.java
@@ -16,7 +16,10 @@ import java.util.HashMap;
 
 import cc.slack.utils.render.ColorUtil;
 import cc.slack.utils.render.RenderUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 
 public class ClassicArrayList implements IArraylist {
 
@@ -25,14 +28,25 @@ public class ClassicArrayList implements IArraylist {
         String second;
     }
 
-
     private final Map<Module, Boolean> moduleStates = new HashMap<>();
     List<Pair> modules = new ArrayList<>();
 
+    private boolean dragging = false;
+    private double dragX = 0, dragY = 0;
+    public float x = 0;
+    public float y = 3;
+
     @Override
     public void onUpdate(UpdateEvent event) {
+
+        if (Slack.getInstance().getModuleManager().getInstance(HUD.class).arraylistResetPos.getValue()) {
+            x = 0;
+            y = 3;
+            Slack.getInstance().getModuleManager().getInstance(HUD.class).arraylistResetPos.setValue(false);
+        }
+
         modules.clear();
-        for (Module module : Slack.getInstance().getModuleManager().getModules() ) {
+        for (Module module : Slack.getInstance().getModuleManager().getModules()) {
             if (!module.render) continue;
             boolean wasEnabled = moduleStates.getOrDefault(module, false);
             boolean isEnabled = module.isToggle();
@@ -42,7 +56,7 @@ public class ClassicArrayList implements IArraylist {
             if (module.isToggle() || !module.disabledTime.hasReached(300)) {
                 String displayName = module.getDisplayName();
                 String mode = module.getMode();
-                String key =  Keyboard.getKeyName(module.getKey());
+                String key = Keyboard.getKeyName(module.getKey());
                 if (mode != null && !mode.isEmpty() && Slack.getInstance().getModuleManager().getInstance(HUD.class).tags.getValue()) {
                     switch (Slack.getInstance().getModuleManager().getInstance(HUD.class).tagsMode.getValue()) {
                         case "(Mode)":
@@ -68,9 +82,11 @@ public class ClassicArrayList implements IArraylist {
                 if (!key.contains("NONE") && Slack.getInstance().getModuleManager().getInstance(HUD.class).binds.getValue()) {
                     switch (Slack.getInstance().getModuleManager().getInstance(HUD.class).bindsMode.getValue()) {
                         case "(Mode)":
-                            displayName += "§7 (" + Keyboard.getKeyName(module.getKey()) + ")";                            break;
+                            displayName += "§7 (" + Keyboard.getKeyName(module.getKey()) + ")";
+                            break;
                         case "[Mode]":
-                            displayName += "§7 [" + Keyboard.getKeyName(module.getKey()) + "]";                            break;
+                            displayName += "§7 [" + Keyboard.getKeyName(module.getKey()) + "]";
+                            break;
                         case "<Mode>":
                             displayName += "§7 <" + Keyboard.getKeyName(module.getKey()) + ">";
                             break;
@@ -106,12 +122,20 @@ public class ClassicArrayList implements IArraylist {
         }
     }
 
-
     @Override
     public void onRender(RenderEvent event) {
-        int y = 3;
+        int currentY = (int) y;
         double c = 0;
         int stringLength = 0;
+
+        ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
+        int mouseX = Mouse.getX() * sr.getScaledWidth() / Minecraft.getMinecraft().displayWidth;
+        int mouseY = sr.getScaledHeight() - Mouse.getY() * sr.getScaledHeight() / Minecraft.getMinecraft().displayHeight - 1;
+
+        if (dragging) {
+            x = (float) (mouseX - dragX);
+            y = (float) (mouseY - dragY);
+        }
 
         for (Pair module : modules) {
             switch (Slack.getInstance().getModuleManager().getInstance(HUD.class).arraylistFont.getValue()) {
@@ -143,33 +167,48 @@ public class ClassicArrayList implements IArraylist {
             switch (Slack.getInstance().getModuleManager().getInstance(HUD.class).arraylistFont.getValue()) {
                 case "Apple":
                     if (Slack.getInstance().getModuleManager().getInstance(HUD.class).arraylistBackground.getValue()) {
-                        drawRoundedRect((int) (event.getWidth() - stringLength * ease - 5), y - 2, (int) (event.getWidth() - stringLength * ease + stringLength + 3) - (int) (event.getWidth() - stringLength * ease - 5), y + Fonts.poppins18.getHeight() + 1 - y - 1, 1.0f, 0x80000000);
+                        drawRoundedRect((float) (x + event.getWidth() - stringLength * ease - 5), currentY - 2, stringLength + 5, Fonts.poppins18.getHeight(), 1.0f, 0x80000000);
                     }
-                    Fonts.apple18.drawStringWithShadow(module.first, event.getWidth() - stringLength * ease - 3, y, ColorUtil.getColor(Slack.getInstance().getModuleManager().getInstance(HUD.class).theme.getValue(), c).getRGB());
-                    y += (int) ((Fonts.apple18.getHeight() + 3) * Math.max(0, (ease + 0.2)/1.2));
+                    Fonts.apple18.drawStringWithShadow(module.first, x + event.getWidth() - stringLength * ease - 3, currentY, ColorUtil.getColor(Slack.getInstance().getModuleManager().getInstance(HUD.class).theme.getValue(), c).getRGB());
+                    currentY += (int) ((Fonts.apple18.getHeight() + 3) * Math.max(0, (ease + 0.2) / 1.2));
                     c += 0.13;
                     break;
                 case "Poppins":
                     if (Slack.getInstance().getModuleManager().getInstance(HUD.class).arraylistBackground.getValue()) {
-                        drawRoundedRect((int) (event.getWidth() - stringLength * ease - 5), y - 2, (int) (event.getWidth() - stringLength * ease + stringLength + 3) - (int) (event.getWidth() - stringLength * ease - 5), y + Fonts.poppins18.getHeight() + 1 - y + 2, 1.0f, 0x80000000);
+                        drawRoundedRect((float) (x + event.getWidth() - stringLength * ease - 5), currentY - 2, stringLength + 5, Fonts.poppins18.getHeight(), 1.0f, 0x80000000);
                     }
-                    Fonts.poppins18.drawStringWithShadow(module.first, event.getWidth() - stringLength * ease - 3, y,  ColorUtil.getColor(Slack.getInstance().getModuleManager().getInstance(HUD.class).theme.getValue(), c).getRGB());
-                    y += (int) ((Fonts.poppins18.getHeight() + 3) * Math.max(0, (ease + 0.2)/1.2));
+                    Fonts.poppins18.drawStringWithShadow(module.first, x + event.getWidth() - stringLength * ease - 3, currentY, ColorUtil.getColor(Slack.getInstance().getModuleManager().getInstance(HUD.class).theme.getValue(), c).getRGB());
+                    currentY += (int) ((Fonts.poppins18.getHeight() + 3) * Math.max(0, (ease + 0.2) / 1.2));
                     c += 0.15;
                     break;
                 case "Roboto":
                     if (Slack.getInstance().getModuleManager().getInstance(HUD.class).arraylistBackground.getValue()) {
-                    drawRoundedRect( (int) (event.getWidth() - stringLength * ease - 5), y - 2, (int) (event.getWidth() - stringLength * ease + stringLength + 3) - (int) (event.getWidth() - stringLength * ease - 5), y + Fonts.poppins18.getHeight() + 1 - y - 1, 1.0f, 0x80000000);
+                        drawRoundedRect((float) (x + event.getWidth() - stringLength * ease - 5), currentY - 2, stringLength + 5, Fonts.roboto18.getHeight(), 1.0f, 0x80000000);
                     }
-                    Fonts.roboto18.drawStringWithShadow(module.first, event.getWidth() - stringLength * ease - 3, y, ColorUtil.getColor(Slack.getInstance().getModuleManager().getInstance(HUD.class).theme.getValue(), c).getRGB());
-                    y += (int) ((Fonts.roboto18.getHeight() + 3) * Math.max(0, (ease + 0.2)/1.2));
+                    Fonts.roboto18.drawStringWithShadow(module.first, x + event.getWidth() - stringLength * ease - 3, currentY, ColorUtil.getColor(Slack.getInstance().getModuleManager().getInstance(HUD.class).theme.getValue(), c).getRGB());
+                    currentY += (int) ((Fonts.roboto18.getHeight() + 3) * Math.max(0, (ease + 0.2) / 1.2));
                     c += 0.13;
                     break;
             }
         }
+
+        handleMouseInput(mouseX, mouseY, x, y, event.getWidth() - x, currentY - y);
     }
 
-
+    private void handleMouseInput(int mouseX, int mouseY, float rectX, float rectY, float rectWidth, float rectHeight) {
+        if (Mouse.isButtonDown(0)) {
+            if (!dragging) {
+                if (mouseX >= rectX && mouseX <= rectX + rectWidth &&
+                        mouseY >= rectY && mouseY <= rectY + rectHeight) {
+                    dragging = true;
+                    dragX = mouseX - rectX;
+                    dragY = mouseY - rectY;
+                }
+            }
+        } else {
+            dragging = false;
+        }
+    }
 
     @Override
     public String toString() {
